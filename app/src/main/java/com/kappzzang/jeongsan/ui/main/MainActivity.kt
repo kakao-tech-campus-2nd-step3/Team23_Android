82426e9e34ac38b89ac5a348b85b0b5cce3bd3fd
Jeong Suhyeon <jsh00325@naver.com>
2024-09-13 15:20:30 +0900

    feat: Set up the Jeongsan Project

---
+++ app/src/main/java/com/kappzzang/jeongsan/ui/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.kappzzang.jeongsan.ui.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.kappzzang.jeongsan.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
