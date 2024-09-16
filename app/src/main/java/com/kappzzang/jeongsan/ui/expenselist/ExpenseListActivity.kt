dc7f42ae7fc4b3346a389c95a4aebf33b6f4face
Jeong Suhyeon <jsh00325@naver.com>
2024-09-16 21:09:22 +0900

    Feat: 지출 목록 페이지 레이아웃(공통 액티비티) 구현
    - 3개의 fragment에서 겹치는 부분을 구현
    - FrameLayout으로 fragment가 들어갈 부분 표시

---
+++ app/src/main/java/com/kappzzang/jeongsan/ui/expenselist/ExpenseListActivity.kt
@@ -0,0 +1,13 @@
+package com.kappzzang.jeongsan.ui.expenselist
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.kappzzang.jeongsan.R
+
+class ExpenseListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_expense_list)
+
+    }
+}
