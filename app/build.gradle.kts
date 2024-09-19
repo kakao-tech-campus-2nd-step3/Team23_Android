fce9164cba6784847cf30bacb5b38f3c6941c664
sumin <cleonn7878@gmail.com>
2024-09-19 17:52:38 +0900

    Feat: 최종 완료 UI 구현
    - 폭죽 이벤트를 위한 라이브러리 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ android {
 
     buildFeatures {
         dataBinding = true
+        viewBinding = true
         buildConfig = true
     }
 }
@@ -75,4 +76,6 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-intents:3.6.1")
     androidTestImplementation("com.google.dagger:hilt-android-testing:2.48.1")
     kaptAndroidTest("com.google.dagger:hilt-android-compiler:2.48.1")
+
+    implementation("nl.dionsegijn:konfetti-xml:2.0.4")
 }
