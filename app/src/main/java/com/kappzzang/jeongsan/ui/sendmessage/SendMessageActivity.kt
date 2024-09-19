456a0a8901924a76c160e361cd069d01b4055dae
sumin <cleonn7878@gmail.com>
2024-09-19 17:50:35 +0900

    Feat: 정산 요청 UI 구현

---
+++ app/src/main/java/com/kappzzang/jeongsan/ui/sendmessage/SendMessageActivity.kt
@@ -0,0 +1,37 @@
+package com.kappzzang.jeongsan.ui.sendmessage
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.kappzzang.jeongsan.R
+import com.kappzzang.jeongsan.databinding.ActivitySendMessageBinding
+import com.kappzzang.jeongsan.ui.Member
+import com.kappzzang.jeongsan.ui.MemberAdapter
+import com.kappzzang.jeongsan.ui.sendcomplete.SendCompleteActivity
+
+class SendMessageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivitySendMessageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.sendMessageButton.setOnClickListener {
+            startActivity(Intent(this, SendCompleteActivity::class.java))
+        }
+
+        val members = mutableListOf<Member>()
+        for (i in 0..3) {
+            members.add(
+                Member("Member$i")
+            )
+        }
+        binding.infoContentReyclerview.apply {
+
+            adapter = MemberAdapter(members.toList(), layoutInflater, R.layout.item_member_expense)
+            layoutManager = LinearLayoutManager(
+                this@SendMessageActivity, LinearLayoutManager.VERTICAL, false
+            )
+        }
+    }
+}
