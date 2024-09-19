245c4bdcb4687b0398c9b6797dc8329b78e8a71f
정수현 <jsh00325@naver.com>
2024-09-20 01:49:27 +0900

    Feat: 메인 페이지 & 지출 내역 페이지 레이아웃 구현 (#4)
    * Feat: 메인 페이지 레이아웃 구현
    
    * Feat: 지출 목록 페이지 레이아웃(공통 액티비티) 구현
    
    - 3개의 fragment에서 겹치는 부분을 구현
    - FrameLayout으로 fragment가 들어갈 부분 표시
    
    * Feat: 지출 목록 페이지의 정산 중 프래그먼트 제작
    
    - ChipGroup을 통해 내가 확인했는지 여부에 따라 나눠서 볼 수 있도록 제작
    
    * Feat: 지출 목록 페이지의 송금 대기 프래그먼트 제작
    
    * Feat: 지출 목록 페이지의 송금 완료 프래그먼트 제작
    
    * Refactor: 레이아웃 ID를 snake_case로 수정
    
    * Chore: Weekly/3에서 변경된 사항 병합
    
    * Design: Dimen을 사용하여 레이아웃 크기 조절
    
    * Design: 레이아웃의 배경을 흰 색으로 지정
    
    * Design: BottomNavigationView의 배경 수정
    
    * Feat: 메인 페이지 Dummy 데이터 넣기
    
    * Fix: ktlint에 따라 코드 수정
    
    * Fix: ktlint에 따라 코드 수정 2

--- app/src/main/java/com/kappzzang/jeongsan/ui/main/MainActivity.kt
+++ app/src/main/java/com/kappzzang/jeongsan/ui/main/MainActivity.kt
@@ -2,11 +2,32 @@ package com.kappzzang.jeongsan.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.kappzzang.jeongsan.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.kappzzang.jeongsan.databinding.ActivityMainBinding
+import com.kappzzang.jeongsan.domain.model.GroupItem
+import com.kappzzang.jeongsan.domain.model.GroupViewType
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 임시 ui 코드
+        binding.userNameTextview.text = "라이언"
+
+        binding.groupListRecyclerview.adapter = GroupListAdapter(createDemoGroupItemList())
+        binding.groupListRecyclerview.layoutManager = LinearLayoutManager(this)
     }
+
+    private fun createDemoGroupItemList(): List<GroupItem> = listOf(
+        GroupItem(GroupViewType.PROGRESS_TITLE, "", listOf()),
+        GroupItem(GroupViewType.GROUP, "캡짱모임", listOf()),
+        GroupItem(GroupViewType.DONE_TITLE, "", listOf()),
+        GroupItem(GroupViewType.GROUP, "모임 이름 1", listOf()),
+        GroupItem(GroupViewType.GROUP, "모임 이름 2", listOf()),
+        GroupItem(GroupViewType.GROUP, "모임 이름 3", listOf())
+    )
 }
