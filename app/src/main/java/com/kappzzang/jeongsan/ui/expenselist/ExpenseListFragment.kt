6d759f8e10f34feb1cbb6eba6e89e13afa7ccd47
Jeong Suhyeon <jsh00325@naver.com>
2024-09-20 15:41:13 +0900

    Feat: 정산 중 프래그먼트에서 dummy 값 추가

--- app/src/main/java/com/kappzzang/jeongsan/ui/expenselist/ExpenseListFragment.kt
+++ app/src/main/java/com/kappzzang/jeongsan/ui/expenselist/ExpenseListFragment.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.kappzzang.jeongsan.databinding.FragmentExpenseListBinding
+import com.kappzzang.jeongsan.domain.model.ExpenseItem
 
 class ExpenseListFragment : Fragment() {
 
@@ -18,5 +20,17 @@ class ExpenseListFragment : Fragment() {
     ): View? {
         binding = FragmentExpenseListBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // UI 확인을 위한 임시 코드
+        binding.expenseListRecyclerview.adapter = ExpenseListAdapter(createDemoExpenseItemList())
+        binding.expenseListRecyclerview.layoutManager = LinearLayoutManager(this.context)
+    }
+
+    private fun createDemoExpenseItemList(): List<ExpenseItem> = (1..9).map { i ->
+        ExpenseItem("정산 중인 내역 $i", "$i,000원", "2024.09.0$i", "#FF0000")
     }
 }
