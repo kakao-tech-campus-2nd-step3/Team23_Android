a68252a908c0fffb7436fe572372a873f4468b63
Jeong Suhyeon <jsh00325@naver.com>
2024-09-20 15:43:55 +0900

    Feat: 송금 완료 프래그먼트에서 dummy 값 추가

--- app/src/main/java/com/kappzzang/jeongsan/ui/expenselist/CompleteExpenseListFragment.kt
+++ app/src/main/java/com/kappzzang/jeongsan/ui/expenselist/CompleteExpenseListFragment.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.kappzzang.jeongsan.databinding.FragmentCompleteExpenseListBinding
+import com.kappzzang.jeongsan.domain.model.ExpenseItem
 
 class CompleteExpenseListFragment : Fragment() {
 
@@ -18,5 +20,18 @@ class CompleteExpenseListFragment : Fragment() {
     ): View? {
         binding = FragmentCompleteExpenseListBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // UI 확인을 위한 임시 코드
+        binding.completeExpenseListRecyclerview.adapter =
+            ExpenseListAdapter(createDemoExpenseItemList())
+        binding.completeExpenseListRecyclerview.layoutManager = LinearLayoutManager(this.context)
+    }
+
+    private fun createDemoExpenseItemList(): List<ExpenseItem> = (1..9).map { i ->
+        ExpenseItem("송금 완료 내역 $i", "$i,000원", "2024.09.0$i", "#0000FF")
     }
 }
