fd4129ea997b269f829afc304dd4a7ea7e4de47e
Jeong Suhyeon <jsh00325@naver.com>
2024-09-20 15:06:05 +0900

    Feat: 하단 네비게이션 바 클릭 시 fragment가 바뀌도록 수정

--- app/src/main/java/com/kappzzang/jeongsan/ui/expenselist/ExpenseListActivity.kt
+++ app/src/main/java/com/kappzzang/jeongsan/ui/expenselist/ExpenseListActivity.kt
@@ -2,11 +2,29 @@ package com.kappzzang.jeongsan.ui.expenselist
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.kappzzang.jeongsan.R
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.kappzzang.jeongsan.databinding.ActivityExpenseListBinding
 
 class ExpenseListActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityExpenseListBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_expense_list)
+        binding = ActivityExpenseListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // UI 확인을 위한 임시 코드
+        binding.groupNameTextview.text = "캡짱모임"
+        binding.totalExpenseTextview.text = "1,000,000원"
+
+        val navHostFragment = supportFragmentManager.findFragmentById(
+            binding.expenseListFragmentcontainerview.id
+        ) as NavHostFragment
+        navController = navHostFragment.navController
+
+        binding.bottomnavigationview.setupWithNavController(navController)
     }
 }
