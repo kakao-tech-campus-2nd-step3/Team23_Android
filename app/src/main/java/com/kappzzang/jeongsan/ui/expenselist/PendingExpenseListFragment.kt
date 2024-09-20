3a5c3ec7b26ed047cf7036db1cc7ec49c6e90c35
Jeong Suhyeon <jsh00325@naver.com>
2024-09-20 15:42:31 +0900

    Feat: 송금 대기 프래그먼트에서 dummy 값 추가

--- app/src/main/java/com/kappzzang/jeongsan/ui/expenselist/PendingExpenseListFragment.kt
+++ app/src/main/java/com/kappzzang/jeongsan/ui/expenselist/PendingExpenseListFragment.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.kappzzang.jeongsan.databinding.FragmentPendingExpenseListBinding
+import com.kappzzang.jeongsan.domain.model.ExpenseItem
 
 class PendingExpenseListFragment : Fragment() {
 
@@ -18,5 +20,18 @@ class PendingExpenseListFragment : Fragment() {
     ): View? {
         binding = FragmentPendingExpenseListBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // UI 확인을 위한 임시 코드
+        binding.pendingExpenseListRecyclerview.adapter =
+            ExpenseListAdapter(createDemoExpenseItemList())
+        binding.pendingExpenseListRecyclerview.layoutManager = LinearLayoutManager(this.context)
+    }
+
+    private fun createDemoExpenseItemList(): List<ExpenseItem> = (1..9).map { i ->
+        ExpenseItem("송금 대기 내역 $i", "$i,000원", "2024.09.0$i", "#00FF00")
     }
 }
