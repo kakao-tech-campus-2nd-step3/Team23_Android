45f01f311ea99fc990d9b3c4f52d79f06de46316
정수현 <jsh00325@naver.com>
2024-09-20 22:50:21 +0900

    Revert "Chore: Weekly 브랜치의 3주차 개발 내용을 Develop 브랜치에 병합 (#12)"
    This reverts commit c46deddccb138e8d88e2d249c396b01d6431e195.

--- app/src/main/java/com/kappzzang/jeongsan/ui/main/GroupViewItem.kt
+++
@@ -1,9 +0,0 @@
-package com.kappzzang.jeongsan.ui.main
-
-data class GroupViewItem(
-    val id: String,
-    val name: String,
-    val isCompleted: Boolean,
-    val profileImageURL: List<String>,
-    val type: GroupViewType
-)
--- app/src/main/java/com/kappzzang/jeongsan/ui/main/GroupViewType.kt
+++
@@ -1,7 +0,0 @@
-package com.kappzzang.jeongsan.ui.main
-
-enum class GroupViewType {
-    PROGRESS_TITLE,
-    DONE_TITLE,
-    GROUP
-}
