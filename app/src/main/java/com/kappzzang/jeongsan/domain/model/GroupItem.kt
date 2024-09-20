f49ce60483699bb26c2fc0f94efab9b5f25a11c2
Jeong Suhyeon <jsh00325@naver.com>
2024-09-20 21:55:19 +0900

    Feat: 일부 도메인 모델(GroupItem, ExpenseItem, MemberItem) 생성

--- app/src/main/java/com/kappzzang/jeongsan/domain/model/GroupItem.kt
+++ app/src/main/java/com/kappzzang/jeongsan/domain/model/GroupItem.kt
@@ -1,7 +1,8 @@
 package com.kappzzang.jeongsan.domain.model
 
 data class GroupItem(
-    val viewType: GroupViewType,
-    val groupName: String,
+    val id: String,
+    val name: String,
+    val isCompleted: Boolean,
     val profileImageURL: List<String>
 )
