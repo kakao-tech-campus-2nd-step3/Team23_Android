ec8a1755e364bdaafefe50a6c9fa4617d507b1bc
sumin <cleonn7878@gmail.com>
2024-09-21 19:35:54 +0900

    Feat: ViewModel 구현

--- app/src/main/java/com/kappzzang/jeongsan/domain/usecase/GetInviteInfoUseCase.kt
+++ app/src/main/java/com/kappzzang/jeongsan/domain/usecase/GetInviteInfoUseCase.kt
@@ -1,9 +1,13 @@
 package com.kappzzang.jeongsan.domain.usecase
 
+import com.kappzzang.jeongsan.domain.model.MemberItem
 import com.kappzzang.jeongsan.domain.repository.MemberRepository
 import javax.inject.Inject
 
 class GetInviteInfoUseCase(
     private val memberRepository: MemberRepository
 ) {
+    suspend operator fun invoke(): List<MemberItem> {
+        return listOf<MemberItem>()
+    }
 }
--- app/src/main/java/com/kappzzang/jeongsan/ui/inviteinfo/InviteInfoViewModel.kt
+++ app/src/main/java/com/kappzzang/jeongsan/ui/inviteinfo/InviteInfoViewModel.kt
@@ -1,16 +1,27 @@
 package com.kappzzang.jeongsan.ui.inviteinfo
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.kappzzang.jeongsan.domain.model.MemberItem
 import com.kappzzang.jeongsan.domain.repository.MemberRepository
 import com.kappzzang.jeongsan.domain.usecase.GetInviteInfoUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class InviteInfoViewModel @Inject constructor(
     private val getInviteInfoUseCase: GetInviteInfoUseCase
-//    private val memberRepository: MemberRepository
 ) : ViewModel() {
-    fun getMember() {
+    private val _inviteInfo = MutableLiveData<List<MemberItem>>()
+    val inviteInfo: LiveData<List<MemberItem>>
+        get() = _inviteInfo
+
+    fun getInviteInfo() {
+        viewModelScope.launch {
+            _inviteInfo.postValue(getInviteInfoUseCase.invoke())
+        }
     }
 }
