61b6c0dc496a9f254afd412437134524e9c5019b
Jeong Suhyeon <jsh00325@naver.com>
2024-09-22 22:42:01 +0900

    Feat: 사용자 이름을 받도록 ViewModel 수정

--- app/src/main/java/com/kappzzang/jeongsan/ui/main/GroupInfoViewModel.kt
+++ app/src/main/java/com/kappzzang/jeongsan/ui/main/GroupInfoViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.kappzzang.jeongsan.domain.usecase.GetDoneGroupUseCase
 import com.kappzzang.jeongsan.domain.usecase.GetProgressingGroupUseCase
+import com.kappzzang.jeongsan.domain.usecase.GetUserNameUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -13,14 +14,25 @@ import kotlinx.coroutines.launch
 @HiltViewModel
 class GroupInfoViewModel @Inject constructor(
     private val getProgressingGroupUseCase: GetProgressingGroupUseCase,
-    private val getDoneGroupUseCase: GetDoneGroupUseCase
+    private val getDoneGroupUseCase: GetDoneGroupUseCase,
+    private val getUserNameUseCase: GetUserNameUseCase
 ) : ViewModel() {
+
+    private val _userName = MutableStateFlow("")
+    val userName: StateFlow<String> = _userName
 
     private val _groupList = MutableStateFlow<List<GroupViewItem>>(emptyList())
     val groupList: StateFlow<List<GroupViewItem>> = _groupList
 
     init {
+        loadUserName()
         loadGroupList()
+    }
+
+    private fun loadUserName() {
+        viewModelScope.launch {
+            _userName.value = getUserNameUseCase()
+        }
     }
 
     private fun loadGroupList() {
