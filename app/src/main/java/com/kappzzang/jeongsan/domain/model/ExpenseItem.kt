c10da554bacb2fcb542d847e912eab9a12658542
ksc1008 <ksc1008@naver.com>
2024-09-23 22:34:43 +0900

    Style: 프로젝트 리포맷 수행

--- app/src/main/java/com/kappzzang/jeongsan/domain/model/ExpenseItem.kt
+++ app/src/main/java/com/kappzzang/jeongsan/domain/model/ExpenseItem.kt
@@ -1,6 +1,5 @@
 package com.kappzzang.jeongsan.domain.model
 
-import com.kappzzang.jeongsan.ui.Member
 import java.util.Date
 
 enum class ExpenseState { CONFIRMED, NOT_CONFIRMED, TRANSFER_PENDING, TRANSFERED }
@@ -8,7 +7,7 @@ enum class ExpenseState { CONFIRMED, NOT_CONFIRMED, TRANSFER_PENDING, TRANSFERED
 data class ExpenseItem(
     val id: String,
     val name: String,
-    val payer: Member,
+    val payer: MemberItem,
     val price: Int,
     val date: Date,
     val state: ExpenseState,
