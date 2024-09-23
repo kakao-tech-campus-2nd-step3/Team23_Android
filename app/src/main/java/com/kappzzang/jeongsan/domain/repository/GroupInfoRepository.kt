480d9e6347af714a1a5722f9f98172d5b567f162
ksc1008 <ksc1008@naver.com>
2024-09-23 22:10:46 +0900

    Feat: GroupInfoRepository 구현

--- app/src/main/java/com/kappzzang/jeongsan/domain/repository/GroupInfoRepository.kt
+++ app/src/main/java/com/kappzzang/jeongsan/domain/repository/GroupInfoRepository.kt
@@ -4,6 +4,13 @@ import com.kappzzang.jeongsan.domain.model.GroupItem
 import kotlinx.coroutines.flow.Flow
 
 interface GroupInfoRepository {
+    /**
+     *  현재 조회중인 그룹 내용 조회
+     *  @param groupId 그룹명
+     *  @return 그룹 정보
+     */
+    fun getGroupInfo(groupId: String): Flow<GroupItem>
+
     suspend fun getProgressingGroupInfo(): List<GroupItem>
     suspend fun getDoneGroupInfo(): List<GroupItem>
 
