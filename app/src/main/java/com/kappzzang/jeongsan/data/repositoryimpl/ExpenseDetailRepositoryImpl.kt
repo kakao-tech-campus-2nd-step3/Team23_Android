3d1b29471720aa37870184ec9950c5ab9674f1db
sumin <cleonn7878@gmail.com>
2024-09-25 01:04:15 +0900

    Feat: ExpenseDetailRepositoryImpl 구현

--- app/src/main/java/com/kappzzang/jeongsan/data/repositoryimpl/ExpenseDetailRepositoryImpl.kt
+++ app/src/main/java/com/kappzzang/jeongsan/data/repositoryimpl/ExpenseDetailRepositoryImpl.kt
@@ -1,8 +1,26 @@
 package com.kappzzang.jeongsan.data.repositoryimpl
 
+import com.kappzzang.jeongsan.domain.model.ExpenseDetailItem
 import com.kappzzang.jeongsan.domain.repository.ExpenseDetailRepository
 import javax.inject.Inject
 
 class ExpenseDetailRepositoryImpl @Inject constructor() :
     ExpenseDetailRepository {
+        fun getExpenseDetail(): List<ExpenseDetailItem> =
+            listOf(
+                ExpenseDetailItem(
+                    "id1",
+                    "아주 맛있는 과자",
+                    4000,
+                    3,
+                    0
+                ),
+                ExpenseDetailItem(
+                    "id2",
+                    "아주 맛없는 고기",
+                    50000,
+                    1,
+                    0
+                ),
+            )
 }
