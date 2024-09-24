3afeff771eeb95791b307da350cfb2f2e7ffa78d
ksc1008 <ksc1008@naver.com>
2024-09-24 17:23:49 +0900

    Refactor: 데이터베이스와 연동되도록 DataSource수정

--- app/src/main/java/com/kappzzang/jeongsan/data/datasource/ExpenseListFakeDatasource.kt
+++ app/src/main/java/com/kappzzang/jeongsan/data/datasource/ExpenseListFakeDatasource.kt
@@ -1,67 +1,50 @@
 package com.kappzzang.jeongsan.data.datasource
 
-import com.kappzzang.jeongsan.domain.model.ExpenseItem
+import com.kappzzang.jeongsan.data.datasource.expense.ExpenseDatabase
+import com.kappzzang.jeongsan.data.mapper.ExpenseEntityMapper
 import com.kappzzang.jeongsan.domain.model.ExpenseListResponse
 import com.kappzzang.jeongsan.domain.model.ExpenseState
-import com.kappzzang.jeongsan.domain.model.MemberItem
-import java.util.Date
 import javax.inject.Inject
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 
-class ExpenseListFakeDatasource @Inject constructor() {
-    private val nameList = listOf("스타벅스", "GS25", "빽다방", "롯데시네마", "CGV", "롯데리아", "버거킹")
-
-    private fun makeFakeItemWithState(expenseState: ExpenseState, prefix: Int): ExpenseItem {
-        val adjustedIndex = (prefix + 1) * (ExpenseState.entries.indexOf(expenseState) + 1)
-
-        return ExpenseItem(
-            id = adjustedIndex.toString(),
-            name = "${nameList[adjustedIndex % nameList.size]}$prefix",
-            payer = MemberItem("123", "test", "", true),
-            price = 1200 * adjustedIndex,
-            date = Date(2024, 9, 1 + prefix % 30, 10, 30),
-            state = expenseState,
-            categoryColor = "#ffbbcc"
-        )
-    }
-
-    private fun makeFakeItemListWithState(
-        count: Int,
-        expenseState: ExpenseState
-    ): List<ExpenseItem> = (1..count).map {
-        makeFakeItemWithState(expenseState, it)
-    }
+class ExpenseListFakeDatasource @Inject constructor(private val expenseDatabase: ExpenseDatabase) {
 
     fun getExpenseData(expenseState: ExpenseState): Flow<ExpenseListResponse> = flow {
+        delay(1000)
         val result =
             when (expenseState) {
-                ExpenseState.CONFIRMED -> ExpenseListResponse(
-                    120000,
-                    0,
-                    makeFakeItemListWithState(10, expenseState)
-                )
+                ExpenseState.CONFIRMED -> expenseDatabase.expenseDao().getConfirmedExpense().map {
+                    ExpenseEntityMapper.mapExpenseEntityToModel(it)
+                }
 
-                ExpenseState.NOT_CONFIRMED -> ExpenseListResponse(
-                    120000,
-                    0,
-                    makeFakeItemListWithState(5, expenseState)
-                )
+                ExpenseState.NOT_CONFIRMED -> expenseDatabase.expenseDao().getNotConfirmedExpense()
+                    .map {
+                        ExpenseEntityMapper.mapExpenseEntityToModel(it)
+                    }
 
-                ExpenseState.TRANSFER_PENDING -> ExpenseListResponse(
-                    120000,
-                    60000,
-                    makeFakeItemListWithState(8, expenseState)
-                )
+                ExpenseState.TRANSFER_PENDING -> expenseDatabase.expenseDao().getPendingExpense()
+                    .map {
+                        ExpenseEntityMapper.mapExpenseEntityToModel(it)
+                    }
 
-                ExpenseState.TRANSFERED -> ExpenseListResponse(
-                    120000,
-                    0,
-                    makeFakeItemListWithState(20, expenseState)
-                )
+                ExpenseState.TRANSFERED -> expenseDatabase.expenseDao().getTransferredExpense()
+                    .map {
+                        ExpenseEntityMapper.mapExpenseEntityToModel(it)
+                    }
             }
-        delay(1500)
-        emit(result)
+        var totalPrice: Int = 0
+
+        result.forEach {
+            totalPrice += it.price
+        }
+
+        val fakeResponse = ExpenseListResponse(
+            totalPrice = totalPrice,
+            expenseList = result,
+            totalExpenseToSend = totalPrice / 2
+        )
+        emit(fakeResponse)
     }
 }
---
+++ app/src/main/java/com/kappzzang/jeongsan/data/mapper/ExpenseEntityMapper.kt
@@ -0,0 +1,21 @@
+package com.kappzzang.jeongsan.data.mapper
+
+import com.kappzzang.jeongsan.data.entity.ExpenseEntity
+import com.kappzzang.jeongsan.domain.model.ExpenseItem
+import com.kappzzang.jeongsan.domain.model.ExpenseState
+import com.kappzzang.jeongsan.domain.model.MemberItem
+import java.sql.Timestamp
+import java.time.LocalDate
+import java.util.Date
+
+object ExpenseEntityMapper {
+    fun mapExpenseEntityToModel(entity: ExpenseEntity): ExpenseItem = ExpenseItem(
+        id = entity.id.toString(),
+        name = entity.name,
+        categoryColor = entity.categoryColor,
+        price = entity.totalPrice,
+        date = Timestamp.valueOf(entity.createdTime),
+        payer = MemberItem("", "", "", false),
+        state = ExpenseState.entries[entity.expenseState]
+    )
+}
