bec341e12a0b61836ee5cff99c421649e7bde5a9
ksc1008 <ksc1008@naver.com>
2024-09-24 15:53:58 +0900

    Fix: 업스트림 리포지토리와 Merge Conflict 해결

--- app/src/main/java/com/kappzzang/jeongsan/di/RepositoryModule.kt
+++ app/src/main/java/com/kappzzang/jeongsan/di/RepositoryModule.kt
@@ -1,7 +1,9 @@
 package com.kappzzang.jeongsan.di
 
+import com.kappzzang.jeongsan.data.repositoryimpl.ExpenseListFakeRepositoryImpl
 import com.kappzzang.jeongsan.data.repositoryimpl.GroupInfoRepositoryImpl
 import com.kappzzang.jeongsan.data.repositoryimpl.UserInfoRepositoryImpl
+import com.kappzzang.jeongsan.domain.repository.ExpenseListRepository
 import com.kappzzang.jeongsan.domain.repository.GroupInfoRepository
 import com.kappzzang.jeongsan.domain.repository.UserInfoRepository
 import com.kappzzang.jeongsan.data.repositoryimpl.MemberRepositoryImpl
@@ -9,6 +11,8 @@ import com.kappzzang.jeongsan.domain.repository.MemberRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.android.scopes.ViewModelScoped
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -27,6 +31,12 @@ abstract class RepositoryModule {
     abstract fun bindUserInfoRepository(
         userInfoRepositoryImpl: UserInfoRepositoryImpl
     ): UserInfoRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindExpensePageRepository(
+        expenseListRepository: ExpenseListFakeRepositoryImpl
+    ): ExpenseListRepository
 
     @Binds
     @Singleton
