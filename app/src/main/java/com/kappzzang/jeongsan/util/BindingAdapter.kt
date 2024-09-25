f6e1a23cbb13104672e7575b68b53e5149451a10
sumin <cleonn7878@gmail.com>
2024-09-25 22:51:44 +0900

    Feat: BindingAdapter에 지출 내역 이미지 표시를 위한 커스텀 속성 추가

--- app/src/main/java/com/kappzzang/jeongsan/util/BindingAdapter.kt
+++ app/src/main/java/com/kappzzang/jeongsan/util/BindingAdapter.kt
@@ -1,12 +1,27 @@
 package com.kappzzang.jeongsan.util
 
 import android.widget.EditText
+import android.widget.ImageView
 import androidx.core.widget.addTextChangedListener
 import androidx.databinding.BindingAdapter
 import androidx.databinding.InverseBindingAdapter
 import androidx.databinding.InverseBindingListener
+import com.bumptech.glide.Glide
 
 object BindingAdapter {
+
+    @JvmStatic
+    @BindingAdapter("expenseImageUrl")
+    fun loadImage(view: ImageView, url: String?) {
+        if (!url.isNullOrEmpty()) {
+            Glide.with(view.context)
+                .load(url)
+                .into(view)
+        } else {
+            // 기본 이미지 또는 처리 로직 추가 가능
+        }
+    }
+
     @BindingAdapter("stringText")
     @JvmStatic
     fun setTextValue(view: EditText, value: String?) {
