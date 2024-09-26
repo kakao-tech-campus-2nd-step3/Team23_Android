22fe2f84f00f5b76d22f09a924afcdaf998c28cb
Jeong Suhyeon <jsh00325@naver.com>
2024-09-26 21:31:41 +0900

    Feat: 콜백을 통해 아이템을 추가, 삭제하도록 설정

--- app/src/main/java/com/kappzzang/jeongsan/ui/addexpense/ExpenseItemListAdapter.kt
+++ app/src/main/java/com/kappzzang/jeongsan/ui/addexpense/ExpenseItemListAdapter.kt
@@ -7,23 +7,39 @@ import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.kappzzang.jeongsan.databinding.ItemExpenseItemBinding
 
-class ExpenseItemListAdapter :
-    ListAdapter<ExpenseItemInput, ExpenseItemListAdapter.ExpenseItemViewHolder>(
-        object :
-            DiffUtil.ItemCallback<ExpenseItemInput>() {
-            override fun areItemsTheSame(
-                oldItem: ExpenseItemInput,
-                newItem: ExpenseItemInput
-            ): Boolean = oldItem === newItem
+class ExpenseItemListAdapter(
+    private val onAddItem: () -> Unit,
+    private val onRemoveItem: (Int) -> Unit
+) : ListAdapter<ExpenseItemInput, ExpenseItemListAdapter.ExpenseItemViewHolder>(
+    object :
+        DiffUtil.ItemCallback<ExpenseItemInput>() {
+        override fun areItemsTheSame(
+            oldItem: ExpenseItemInput,
+            newItem: ExpenseItemInput
+        ): Boolean = oldItem === newItem
 
-            override fun areContentsTheSame(
-                oldItem: ExpenseItemInput,
-                newItem: ExpenseItemInput
-            ): Boolean = oldItem == newItem
-        }
-    ) {
-    class ExpenseItemViewHolder(private val binding: ItemExpenseItemBinding) :
+        override fun areContentsTheSame(
+            oldItem: ExpenseItemInput,
+            newItem: ExpenseItemInput
+        ): Boolean = oldItem == newItem
+    }
+) {
+
+    inner class ExpenseItemViewHolder(private val binding: ItemExpenseItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
+        init {
+            binding.expenseAddRemoveButton.setOnClickListener {
+                val isLastItem = bindingAdapterPosition + 1 == bindingAdapter?.itemCount
+
+                if (isLastItem) {
+                    // TODO: 아이템 추가 시, 버튼의 모습이 올바르게 변하지 않음...
+                    onAddItem()
+                } else {
+                    onRemoveItem(bindingAdapterPosition)
+                }
+            }
+        }
+
         fun bind(item: ExpenseItemInput) {
             binding.item = item
             binding.isPlaceholder =
