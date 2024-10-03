1925774128992a2d67b84227b0c12294a8998cd6
ksc1008 <ksc1008@naver.com>
2024-10-04 04:21:34 +0900

    Fix: Room 관련 Dependency 수정

--- data/expense/build.gradle.kts
+++ data/expense/build.gradle.kts
@@ -3,5 +3,6 @@ android {
 }
 dependencies {
     implementation(project(":domain:expense"))
-    implementation("androidx.room:room-common:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+    testImplementation("androidx.room:room-testing:2.6.1")
 }
