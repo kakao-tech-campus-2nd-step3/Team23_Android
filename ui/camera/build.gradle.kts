7f74fdab1f49142595e71bf77f02ac948cf39f2a
ksc1008 <ksc1008@naver.com>
2024-10-04 22:08:36 +0900

    Fix: 카메라 의존성 오류 해결

--- ui/camera/build.gradle.kts
+++ ui/camera/build.gradle.kts
@@ -4,9 +4,10 @@ android {
 
 dependencies {
     implementation(project(":domain:ocr"))
-    implementation("androidx.camera:camera-core:1.3.4")
     implementation("com.github.bumptech.glide:glide:4.14.2")
+    implementation("androidx.camera:camera-core:1.3.4")
     implementation("androidx.camera:camera-lifecycle:1.3.4")
     implementation("androidx.camera:camera-view:1.3.4")
+    implementation("androidx.camera:camera-camera2:1.3.4")
     implementation(project(":ui:data"))
 }
