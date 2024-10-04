316d7f5ffee7ca8735f0d29d28f5fc0a71b60b94
ksc1008 <ksc1008@naver.com>
2024-10-04 16:23:52 +0900

    Feat: 예시 코드 작성

--- ui/login/src/main/java/com/kappzzang/jeongsan/login/LoginActivity.kt
+++ ui/login/src/main/java/com/kappzzang/jeongsan/login/LoginActivity.kt
@@ -3,16 +3,23 @@ package com.kappzzang.jeongsan.login
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.kappzzang.jeongsan.login.databinding.ActivityLoginBinding
+import com.kappzzang.jeongsan.navigation.AppNavigator
+import javax.inject.Inject
 
 class LoginActivity : AppCompatActivity() {
+    @Inject
+    lateinit var appNavigator: AppNavigator
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
         binding.loginByKakaoImagebutton.setOnClickListener {
             // TODO: 카카오 로그인 구현하기
-            // startActivity(Intent(this, MainActivity::class.java))
+
+            startActivity(appNavigator.navigateToMainPage())
         }
     }
 }
