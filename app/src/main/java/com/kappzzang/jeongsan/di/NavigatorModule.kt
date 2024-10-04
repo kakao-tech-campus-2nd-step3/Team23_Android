6bce1d3940e4d40484663a34980998eb5443d5e5
ksc1008 <ksc1008@naver.com>
2024-10-04 22:18:24 +0900

    Refactor: Context Injection 제거

--- app/src/main/java/com/kappzzang/jeongsan/di/NavigatorModule.kt
+++ app/src/main/java/com/kappzzang/jeongsan/di/NavigatorModule.kt
@@ -1,21 +1,18 @@
 package com.kappzzang.jeongsan.di
 
-import android.content.Context
 import com.kappzzang.jeongsan.navigation.AppNavigator
 import com.kappzzang.jeongsan.navigation.NavigatorImpl
+import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object NavigatorModule {
+abstract class NavigatorModule {
 
-    @Provides
+    @Binds
     @Singleton
-    fun provideAppNavigator(@ApplicationContext context: Context): AppNavigator =
-        NavigatorImpl(context)
+    abstract fun bindAppNavigator(appNavigatorImpl: NavigatorImpl): AppNavigator
 }
