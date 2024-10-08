7626e77dd867d7bf6b53431bfd99bff26a644dec
Jeong Suhyeon <jsh00325@naver.com>
2024-10-08 22:40:01 +0900

    Feat: 그룹 더미 데이터를 주입하는 코드 삭제

--- data/group/src/main/java/com/kappzzang/jeongsan/datasource/group/GroupDatabase.kt
+++ data/group/src/main/java/com/kappzzang/jeongsan/datasource/group/GroupDatabase.kt
@@ -4,10 +4,7 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import androidx.sqlite.db.SupportSQLiteDatabase
 import com.kappzzang.jeongsan.entity.GroupEntity
-import java.util.concurrent.Executors
-import kotlinx.coroutines.runBlocking
 
 @Database(entities = [GroupEntity::class], version = 1)
 abstract class GroupDatabase : RoomDatabase() {
@@ -18,47 +15,6 @@ abstract class GroupDatabase : RoomDatabase() {
 
         fun getInstance(context: Context): GroupDatabase = Room
             .databaseBuilder(context, GroupDatabase::class.java, GroupContract.DATABASE_NAME)
-            .addCallback(object : Callback() {
-                override fun onCreate(db: SupportSQLiteDatabase) {
-                    super.onCreate(db)
-
-                    val tempImageUrl = "https://avatars.githubusercontent.com/u/38340588?v=4"
-                    val dummyData = listOf(
-                        GroupEntity(
-                            name = "캡짱모임",
-                            isCompleted = false,
-                            subject = "✈️",
-                            memberProfileImage = tempImageUrl
-                        ),
-                        GroupEntity(
-                            name = "모임 01",
-                            isCompleted = true,
-                            subject = "😊",
-                            memberProfileImage = tempImageUrl
-                        ),
-                        GroupEntity(
-                            name = "모임 02",
-                            isCompleted = false,
-                            subject = "😊",
-                            memberProfileImage = tempImageUrl
-                        ),
-                        GroupEntity(
-                            name = "모임 03",
-                            isCompleted = true,
-                            subject = "✈️",
-                            memberProfileImage = ""
-                        )
-                    )
-
-                    Executors.newSingleThreadExecutor().execute {
-                        runBlocking {
-                            getInstance(context).groupDao().apply {
-                                dummyData.forEach { addGroup(it) }
-                            }
-                        }
-                    }
-                }
-            })
             .build()
     }
 }
