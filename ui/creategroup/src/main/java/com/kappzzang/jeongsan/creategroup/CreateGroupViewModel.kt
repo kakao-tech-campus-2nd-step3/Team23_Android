31c119734c1840ad690801b270352b4ecc24dd0b
Jeong Suhyeon <jsh00325@naver.com>
2024-10-09 01:24:32 +0900

    Feat: 그룹 정보를 검증하고 저장하는 기능 구현

--- ui/creategroup/src/main/java/com/kappzzang/jeongsan/creategroup/CreateGroupViewModel.kt
+++ ui/creategroup/src/main/java/com/kappzzang/jeongsan/creategroup/CreateGroupViewModel.kt
@@ -3,8 +3,10 @@ package com.kappzzang.jeongsan.creategroup
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.kappzzang.jeongsan.data.MemberUIData
+import com.kappzzang.jeongsan.model.GroupCreateItem
 import com.kappzzang.jeongsan.usecase.UploadGroupInfoUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import javax.inject.Inject
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -26,13 +28,19 @@ class CreateGroupViewModel @Inject constructor(
 
     fun updateGroupName(name: String) {
         viewModelScope.launch {
-            _groupName.value = name
+            _groupName.emit(name)
         }
     }
 
     fun updateGroupSubject(subject: String) {
         viewModelScope.launch {
-            _groupSubject.value = subject
+            _groupSubject.emit(subject)
+        }
+    }
+
+    fun updateGroupMemberList(memberList: List<MemberUIData>) {
+        viewModelScope.launch {
+            _groupMemberList.emit(memberList)
         }
     }
 
@@ -46,5 +54,26 @@ class CreateGroupViewModel @Inject constructor(
                 _groupMemberList.value.filterIndexed { index, _ -> index != removedMemberPosition }
             )
         }
+    }
+
+    fun uploadGroupInfo(): Boolean {
+        if (!checkGroupInfoValidation()) {
+            return false
+        }
+
+        val groupInfo = GroupCreateItem(
+            name = _groupName.value,
+            subject = _groupSubject.value,
+            memberIdList = _groupMemberList.value.map { it.uuid }
+        )
+
+        viewModelScope.launch(Dispatchers.IO) {
+            uploadGroupInfoUseCase(groupInfo)
+        }
+        return true
+    }
+
+    private fun checkGroupInfoValidation(): Boolean {
+        return _groupName.value.isNotEmpty() && _groupSubject.value.isNotEmpty() && _groupMemberList.value.isNotEmpty()
     }
 }
