3fcdf6fda72047decf6fb7874b97122ad7efb9a1
Jeong Suhyeon <jsh00325@naver.com>
2024-10-09 00:51:02 +0900

    Chore: Lint 수행

--- ui/creategroup/src/main/java/com/kappzzang/jeongsan/creategroup/MemberAdapter.kt
+++ ui/creategroup/src/main/java/com/kappzzang/jeongsan/creategroup/MemberAdapter.kt
@@ -9,9 +9,8 @@ import com.bumptech.glide.Glide
 import com.kappzzang.jeongsan.creategroup.databinding.ItemMemberInviteBinding
 import com.kappzzang.jeongsan.data.MemberUIData
 
-class MemberAdapter(
-    private val onDeleteMember: (Int) -> Unit
-) : ListAdapter<MemberUIData, MemberAdapter.MemberViewHolder>(diffUtil) {
+class MemberAdapter(private val onDeleteMember: (Int) -> Unit) :
+    ListAdapter<MemberUIData, MemberAdapter.MemberViewHolder>(diffUtil) {
 
     inner class MemberViewHolder(private val binding: ItemMemberInviteBinding) :
         RecyclerView.ViewHolder(binding.root) {
