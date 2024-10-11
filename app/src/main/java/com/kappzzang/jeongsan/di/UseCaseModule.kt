8460b0dd6dfcc42be629edbd2fb26de8fa588016
권성찬 <54511614+ksc1008@users.noreply.github.com>
2024-10-11 13:55:36 +0900

    Feat: 로그인 기능 구현 (#50)
    * Feat: 기본 구조 생성
    
    * Feat: 기본 구조 생성
    
    * Feat: Kakao Sdk 의존성 추가 & BuildConfig 설정
    
    * Feat: 인증 관련 도메인 로직 구현
    
    * Feat: 앱에 KakaoSdk 연동
    
    * Feat: DataStore 구현
    
    * Feat: refreshKakaoToken, Datastore 구현
    
    * Feat: LoginActivity에서 login UI 로직 구현
    
    * Feat: LoginActivity에서 login UI 로직 세부 구현
    
    * Feat: ViewModel과 Activity 바인딩
    
    * Feat: Hilt DI 모듈 추가
    
    * Fix: 카카오로 로그인 버튼 누를 시 정상적으로 상태가 변경되게 ViewModel 로직 수정
    
    * Feat: 카카오 API와 연동하여 로그인 기능 구현
    
    * Fix: Refresh 로직 픽스
    
    * Chore: Lint 수행
    
    * Chore: 불필요한 로그 제거
    
    ---------
    
    Co-authored-by: 정수현 <jsh00325@naver.com>

--- app/src/main/java/com/kappzzang/jeongsan/di/UseCaseModule.kt
+++ app/src/main/java/com/kappzzang/jeongsan/di/UseCaseModule.kt
@@ -6,7 +6,6 @@ import com.kappzzang.jeongsan.repository.GroupInfoRepository
 import com.kappzzang.jeongsan.repository.KakaoAuthenticationRepository
 import com.kappzzang.jeongsan.repository.MemberRepository
 import com.kappzzang.jeongsan.repository.ReceiptRepository
-import com.kappzzang.jeongsan.repository.TransferRepository
 import com.kappzzang.jeongsan.repository.UserInfoRepository
 import com.kappzzang.jeongsan.usecase.AuthenticateWithKakaoUseCase
 import com.kappzzang.jeongsan.usecase.AuthorizeWithKakaoUseCase
@@ -16,12 +15,9 @@ import com.kappzzang.jeongsan.usecase.GetExpenseDetailUseCase
 import com.kappzzang.jeongsan.usecase.GetExpenseUseCase
 import com.kappzzang.jeongsan.usecase.GetInviteInfoUseCase
 import com.kappzzang.jeongsan.usecase.GetProgressingGroupUseCase
-import com.kappzzang.jeongsan.usecase.GetTransferInfoUseCase
 import com.kappzzang.jeongsan.usecase.GetUserInfoUseCase
 import com.kappzzang.jeongsan.usecase.RegisterWithKakaoUseCase
-import com.kappzzang.jeongsan.usecase.SendTransferMessageUseCase
 import com.kappzzang.jeongsan.usecase.UploadExpenseUseCase
-import com.kappzzang.jeongsan.usecase.UploadGroupInfoUseCase
 import com.kappzzang.jeongsan.util.AuthenticationRepository
 import dagger.Module
 import dagger.Provides
@@ -76,17 +72,4 @@ object UseCaseModule {
 
     @Provides
     fun registerWithKakaoUseCase() = RegisterWithKakaoUseCase()
-
-    @Provides
-    fun provideUploadGroupInfoUseCase(groupInfoRepository: GroupInfoRepository) =
-        UploadGroupInfoUseCase(groupInfoRepository)
-
-    fun provideGetTransferInfoUseCase(transferRepository: TransferRepository) =
-        GetTransferInfoUseCase(transferRepository)
-
-    @Provides
-    fun provideSendTransferMessageUseCase(
-        userInfoRepository: UserInfoRepository,
-        transferRepository: TransferRepository
-    ) = SendTransferMessageUseCase(userInfoRepository, transferRepository)
 }
