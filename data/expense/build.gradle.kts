c604d08c2557c6b6c9ef4f2b49cafb71b15a2b46
Jeong Suhyeon <jsh00325@naver.com>
2024-10-11 16:23:55 +0900

    Feat: TalkApiClient를 통해 메시지 전송하는 기능 구현

--- data/expense/build.gradle.kts
+++ data/expense/build.gradle.kts
@@ -4,5 +4,6 @@ android {
 dependencies {
     implementation(project(":domain:expense"))
     implementation("androidx.room:room-ktx:2.6.1")
+    implementation("com.kakao.sdk:v2-talk:2.20.6")
     testImplementation("androidx.room:room-testing:2.6.1")
 }
--- data/expense/src/main/java/com/kappzzang/jeongsan/repositoryimpl/TransferRepositoryImpl.kt
+++ data/expense/src/main/java/com/kappzzang/jeongsan/repositoryimpl/TransferRepositoryImpl.kt
@@ -1,8 +1,13 @@
 package com.kappzzang.jeongsan.repositoryimpl
 
+import android.util.Log
+import com.kakao.sdk.talk.TalkApiClient
 import com.kappzzang.jeongsan.model.TransferDetailItem
 import com.kappzzang.jeongsan.repository.TransferRepository
+import com.kappzzang.jeongsan.util.IntegerFormatter.formatDecimalSeparator
 import javax.inject.Inject
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
 class TransferRepositoryImpl @Inject constructor() : TransferRepository {
     override suspend fun getTransferInfo(): List<TransferDetailItem> {
@@ -27,5 +32,41 @@ class TransferRepositoryImpl @Inject constructor() : TransferRepository {
                 "https://t4.daumcdn.net/thumb/R720x0.fjpg/?fname=http://t1.daumcdn.net/brunch/service/user/cnoC/image/r5RjC7tUqK6Mb4NVX0f5d-qrCSQ.jpg"
             )
         )
+    }
+
+    override suspend fun getTransferLink(memberUuid: String): String? {
+        // TODO: 일단 임시 송금 링크 반환
+        return "https://link.kakaopay.com/_/f1Rxe4x"
+    }
+
+    override suspend fun sendTransferMessage(
+        transferInfoList: List<TransferDetailItem>,
+        transferLink: String,
+        payeeName: String
+    ): Boolean {
+        // TODO: 정보를 보낼 친구의 UUID를 현재 알 수 없으므로, 나에게 보내기로 확인 (첫번째 값으로 메시지)
+        return suspendCoroutine { continuation ->
+            TalkApiClient.instance.sendCustomMemo(
+                templateId = TRANSFER_MESSAGE_TEMPLATE_ID,
+                templateArgs = mapOf(
+                    "price" to transferInfoList[0].fee.formatDecimalSeparator() + "원",
+                    "payee" to payeeName,
+                    "link" to transferLink
+                )
+            ) { error ->
+                if (error != null) {
+                    Log.e(TAG, "송금 메시지 전송 실패", error)
+                    continuation.resume(false)
+                } else {
+                    Log.i(TAG, "송금 메시지 전송 성공")
+                    continuation.resume(true)
+                }
+            }
+        }
+    }
+
+    companion object {
+        private const val TRANSFER_MESSAGE_TEMPLATE_ID = 112933L
+        private const val TAG = "TransferRepositoryImpl"
     }
 }
