e3fcd41c0a0f95b7a4188ab6fe8b8a9814ed9175
정수현 <jsh00325@naver.com>
2024-10-16 09:54:14 +0900

    Refactor: 초대 현황 페이지를 Dialog로 수정 (#61)
    * Refactor: 기존의 액티비티를 DialogFragment로 수정
    
    * Design: 일부 UI 요소들 변경
    
    - dialog의 닫힘 버튼 크기 조정
    - dialog의 배경 모서리를 와이어프레임과 같이 둥글게 변경
    - dialog의 내부 padding 조정
    
    * Refactor: ViewModel에서 Flow를 사용하도록 수정
    
    * Refactor: 초대 현황 클릭 시 Dialog가 실행되도록 수정

--- ui/expenselist/src/main/java/com/kappzzang/jeongsan/expenselist/inviteinfo/InviteInfoViewModel.kt
+++ ui/expenselist/src/main/java/com/kappzzang/jeongsan/expenselist/inviteinfo/InviteInfoViewModel.kt
@@ -4,18 +4,15 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.kappzzang.jeongsan.model.MemberItem
 import com.kappzzang.jeongsan.usecase.GetInviteInfoUseCase
-import com.kappzzang.jeongsan.usecase.SendInviteMessageUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
 @HiltViewModel
 class InviteInfoViewModel @Inject constructor(
-    private val getInviteInfoUseCase: GetInviteInfoUseCase,
-    private val sendInviteMessageUseCase: SendInviteMessageUseCase
+    private val getInviteInfoUseCase: GetInviteInfoUseCase
 ) : ViewModel() {
 
     private val _inviteInfo = MutableStateFlow<List<MemberItem>>(emptyList())
@@ -23,14 +20,9 @@ class InviteInfoViewModel @Inject constructor(
 
     init {
         // 더미 데이터 삽입 & 적용
-        viewModelScope.launch(Dispatchers.IO) {
+        viewModelScope.launch {
             getInviteInfoUseCase.insertDummyData()
             _inviteInfo.emit(getInviteInfoUseCase())
         }
     }
-
-    fun sendInviteMessage(groupId: String, groupName: String, memberId: String) =
-        viewModelScope.launch {
-            sendInviteMessageUseCase.invoke(groupId, groupName, memberId)
-        }
 }
