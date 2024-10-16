9c9058c406177cd94d52c03a86fd4b94ffdee661
정수현 <jsh00325@naver.com>
2024-10-16 09:54:14 +0900

    Refactor: 초대 현황 페이지를 Dialog로 수정 (#61)
    * Refactor: 기존의 액티비티를 DialogFragment로 수정
    
    * Design: 일부 UI 요소들 변경
    
    - dialog의 닫힘 버튼 크기 조정
    - dialog의 배경 모서리를 와이어프레임과 같이 둥글게 변경
    - dialog의 내부 padding 조정
    
    * Refactor: ViewModel에서 Flow를 사용하도록 수정
    
    * Refactor: 초대 현황 클릭 시 Dialog가 실행되도록 수정

---
+++ ui/expenselist/src/main/java/com/kappzzang/jeongsan/expenselist/inviteinfo/InviteInfoDialogFragment.kt
@@ -0,0 +1,67 @@
+package com.kappzzang.jeongsan.expenselist.inviteinfo
+
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.kappzzang.jeongsan.expenselist.databinding.FragmentInviteInfoDialogBinding
+import kotlinx.coroutines.launch
+
+class InviteInfoDialogFragment : DialogFragment() {
+
+    private val viewModel: InviteInfoViewModel by activityViewModels()
+    private lateinit var binding: FragmentInviteInfoDialogBinding
+    private lateinit var memberAdapter: MemberInfoAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentInviteInfoDialogBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setDialogStyle()
+        initRecyclerView()
+        setCloseButton()
+    }
+
+    private fun setDialogStyle() {
+        dialog?.window?.let { window ->
+            window.setBackgroundDrawable(ColorDrawable(android.graphics.Color.TRANSPARENT))
+
+            val width = (resources.displayMetrics.widthPixels * 0.9).toInt()
+            val height = (resources.displayMetrics.heightPixels * 0.9).toInt()
+            window.setLayout(width, height)
+        }
+    }
+
+    private fun initRecyclerView() {
+        memberAdapter = MemberInfoAdapter()
+        binding.memberContentRecyclerview.apply {
+            adapter = memberAdapter
+            layoutManager = LinearLayoutManager(context)
+        }
+
+        lifecycleScope.launch {
+            viewModel.inviteInfo.collect { inviteInfo ->
+                memberAdapter.submitList(inviteInfo)
+            }
+        }
+    }
+
+    private fun setCloseButton() {
+        binding.closeImageview.setOnClickListener {
+            dismiss()
+        }
+    }
+}
--- ui/expenselist/src/main/java/com/kappzzang/jeongsan/expenselist/inviteinfo/InviteInfoViewModel.kt
+++ ui/expenselist/src/main/java/com/kappzzang/jeongsan/expenselist/inviteinfo/InviteInfoViewModel.kt
@@ -1,33 +1,28 @@
 package com.kappzzang.jeongsan.expenselist.inviteinfo
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.kappzzang.jeongsan.model.MemberItem
 import com.kappzzang.jeongsan.usecase.GetInviteInfoUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
 @HiltViewModel
 class InviteInfoViewModel @Inject constructor(
     private val getInviteInfoUseCase: GetInviteInfoUseCase
 ) : ViewModel() {
-    private val _inviteInfo = MutableLiveData<List<MemberItem>>()
-    val inviteInfo: LiveData<List<MemberItem>>
-        get() = _inviteInfo
+
+    private val _inviteInfo = MutableStateFlow<List<MemberItem>>(emptyList())
+    val inviteInfo: StateFlow<List<MemberItem>> = _inviteInfo
 
     init {
-        // 더미 데이터 삽입
+        // 더미 데이터 삽입 & 적용
         viewModelScope.launch {
             getInviteInfoUseCase.insertDummyData()
-        }
-    }
-
-    fun getInviteInfo() {
-        viewModelScope.launch {
-            _inviteInfo.postValue(getInviteInfoUseCase.invoke())
+            _inviteInfo.emit(getInviteInfoUseCase())
         }
     }
 }
