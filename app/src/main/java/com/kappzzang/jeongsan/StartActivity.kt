af6d2707909bca0a2a55609fa92d745303d8c0e9
sumin <cleonn7878@gmail.com>
2024-10-18 00:54:33 +0900

    Feat: StartActivity에서 kakaolink를 사용하는 여러 쿼리 파라미터를 처리 할 수 있도록 구현

--- app/src/main/java/com/kappzzang/jeongsan/StartActivity.kt
+++ app/src/main/java/com/kappzzang/jeongsan/StartActivity.kt
@@ -1,6 +1,9 @@
 package com.kappzzang.jeongsan
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.kappzzang.jeongsan.navigation.AppNavigator
 import dagger.hilt.android.AndroidEntryPoint
@@ -13,12 +16,42 @@ class StartActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        Log.d(TAG, "stared")
         // TODO: 임시로 바로 로그인 페이지로 이동
         // 이후 로그인 정보를 통해 바로 메인 페이지로 보내거나 결정하는 로직
-        appNavigator.navigateToLogin(this).also {
-            startActivity(it)
-            finish()
+        handleIntentData(intent)
+    }
+    private fun handleIntentData(intent: Intent?) {
+        val transferLink = intent?.data?.getQueryParameter("transfer_link")
+        val inviteLink = intent?.data?.getQueryParameter("invite_link")
+
+        when {
+            transferLink != null -> {
+                val transferLinkIntent = Intent(Intent.ACTION_VIEW)
+                transferLinkIntent.data = Uri.parse(transferLink)
+                Log.d(TAG, transferLink)
+                startActivity(transferLinkIntent)
+                finish()
+            }
+            inviteLink != null -> {
+                val inviteLinkIntent = Intent(Intent.ACTION_VIEW)
+                inviteLinkIntent.data = Uri.parse(inviteLink)
+                Log.d(TAG, inviteLink)
+                appNavigator.navigateToLogin(this).also {
+                    startActivity(it)
+                    finish()
+                }
+            }
+            else -> {
+                appNavigator.navigateToLogin(this).also {
+                    startActivity(it)
+                    finish()
+                }
+            }
         }
+    }
+
+    companion object {
+        private const val TAG = "StartActivity"
     }
 }
