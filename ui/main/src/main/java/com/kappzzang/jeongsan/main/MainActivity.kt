a0661b6f23315255a46de9847476e0aef8d45695
sumin <cleonn7878@gmail.com>
2024-10-18 13:47:26 +0900

    Refactor: Lint 수행

--- ui/main/src/main/java/com/kappzzang/jeongsan/main/MainActivity.kt
+++ ui/main/src/main/java/com/kappzzang/jeongsan/main/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.kappzzang.jeongsan.main
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AlertDialog
