07135505b611c61f5b71154f5e97e7568e182534
sumin <cleonn7878@gmail.com>
2024-10-18 17:28:42 +0900

    Refactor: #21 의 리뷰요청 사항 반영(Repository 통합)

--- data/expense/src/main/java/com/kappzzang/jeongsan/di/ExpenseRepositoryModule.kt
+++ data/expense/src/main/java/com/kappzzang/jeongsan/di/ExpenseRepositoryModule.kt
@@ -1,13 +1,11 @@
 package com.kappzzang.jeongsan.di
 
 import com.kappzzang.jeongsan.repository.ExpenseDetailRepository
-import com.kappzzang.jeongsan.repository.ExpenseListRepository
 import com.kappzzang.jeongsan.repository.ExpenseRepository
 import com.kappzzang.jeongsan.repository.ReceiptRepository
 import com.kappzzang.jeongsan.repository.TransferRepository
 import com.kappzzang.jeongsan.repositoryimpl.ExpenseDetailRepositoryImpl
-import com.kappzzang.jeongsan.repositoryimpl.ExpenseListFakeRepositoryImpl
-import com.kappzzang.jeongsan.repositoryimpl.ExpenseRepositoryImpl
+import com.kappzzang.jeongsan.repositoryimpl.ExpenseFakeRepositoryImpl
 import com.kappzzang.jeongsan.repositoryimpl.ReceiptRepositoryImpl
 import com.kappzzang.jeongsan.repositoryimpl.TransferRepositoryImpl
 import dagger.Binds
@@ -21,21 +19,15 @@ import javax.inject.Singleton
 abstract class ExpenseRepositoryModule {
     @Binds
     @Singleton
-    abstract fun bindExpensePageRepository(
-        expenseListRepository: ExpenseListFakeRepositoryImpl
-    ): ExpenseListRepository
+    abstract fun bindExpenseRepository(
+        expenseListRepository: ExpenseFakeRepositoryImpl
+    ): ExpenseRepository
 
     @Binds
     @Singleton
     abstract fun bindExpenseDetailRepository(
         expenseDetailRepositoryImpl: ExpenseDetailRepositoryImpl
     ): ExpenseDetailRepository
-
-    @Binds
-    @Singleton
-    abstract fun bindExpenseRepository(
-        expenseRepositoryImpl: ExpenseRepositoryImpl
-    ): ExpenseRepository
 
     @Binds
     @Singleton
--- data/expense/src/main/java/com/kappzzang/jeongsan/repositoryimpl/ExpenseFakeRepositoryImpl.kt
+++ data/expense/src/main/java/com/kappzzang/jeongsan/repositoryimpl/ExpenseFakeRepositoryImpl.kt
@@ -1,18 +1,20 @@
 package com.kappzzang.jeongsan.repositoryimpl
 
 import com.kappzzang.jeongsan.datasource.ExpenseListFakeDatasource
+import com.kappzzang.jeongsan.model.ExpenseItem
 import com.kappzzang.jeongsan.model.ExpenseListResponse
 import com.kappzzang.jeongsan.model.ExpenseState
-import com.kappzzang.jeongsan.repository.ExpenseListRepository
+import com.kappzzang.jeongsan.repository.ExpenseRepository
 import javax.inject.Inject
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import java.util.Date
 
 data class ExpenseListCachingKey(val expenseState: ExpenseState, val groupId: String)
 
-class ExpenseListFakeRepositoryImpl @Inject constructor(
+class ExpenseFakeRepositoryImpl @Inject constructor(
     private val dataSource: ExpenseListFakeDatasource
-) : ExpenseListRepository {
+) : ExpenseRepository {
 
     private val cachedData = HashMap<ExpenseListCachingKey, ExpenseListResponse>()
 
@@ -36,4 +38,19 @@ class ExpenseListFakeRepositoryImpl @Inject constructor(
             )
         )
     }
+
+    override suspend fun getExpense(id: Long) = ExpenseItem(
+        id = "id",
+        name = "지출 이름입니당",
+        payerName = "돈 많은 사람",
+        payerMemberId = "id",
+        price = 15800,
+        // 임시 지출 이미지 주소 (카카오테크 캠퍼스)
+        expenseImageUrl = "https://www.kakaotechcampus.com/fileUpDownload/" +
+                "download.do?p_savefile=gatepage_20230330053504999_1.png&p_realfile=" +
+                "GNB+%EB%A1%9C%EA%B3%A0%28%EB%B3%B4%EB%9D%BC%29.png",
+        date = Date(),
+        state = ExpenseState.NOT_CONFIRMED,
+        categoryColor = "#FFFFFF"
+    )
 }
