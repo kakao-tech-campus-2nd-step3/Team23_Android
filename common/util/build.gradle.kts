94a57a661806f4096289b03f08ed4c99b934d771
권성찬 <54511614+ksc1008@users.noreply.github.com>
2024-10-24 03:30:13 +0900

    Test: 테스트 코드 작성 (#69)
    * Chore: 테스트 수행을 위한 의존성 추가
    
    * Fix: Native Kotlin에서 테스트 실행을 위해 로깅 함수 제거
    
    * Chore: AssertJ 의존성 추가
    
    * Test: SendTransferMessageUseCase 테스트 코드 작성
    
    * Fix: ExpenseListFakeDatasource 내 Delay 함수 제거
    
    * Test: ExpenseEntityMapper 테스트 코드 작성
    
    * Feat: 날짜를 변환하는 DateConverter 유틸 클래스 추가
    
    * Feat: ExpenseItem의 Date 형식 변경, 매핑하는 메소드 수정
    
    * Test: ExpenseEntityMapper, IntegerFormatter 테스트 코드 추가
    
    * Feat: LocalDateTime을 서버에 저장되는 포맷으로 변환하는 유틸 메소드 추가
    
    * Refactor: OcrResultResponse의 paymentTime 자료형 LocalDateTime으로 변경
    
    * Test: OcrResultEntityMapper 테스트코드 추가
    
    * Chore: Lint 수행
    
    * Refactor: 테스트 코드 패키지 수정
    
    * Test: ReceiptCaptureRepository 테스트 코드 추가
    
    * Chore: Lint 수행
    
    * Fix: 날짜 데이터를 파싱할 때 시차를 무시하게 수정
    
    * Fix: Merge Conflict 해결

--- common/util/build.gradle.kts
+++ common/util/build.gradle.kts
@@ -3,6 +3,12 @@ plugins {
     id("org.jetbrains.kotlin.jvm")
 }
 
+dependencies {
+    // Test Dependencies
+    testImplementation("org.assertj:assertj-core:3.25.3")
+    testImplementation("junit:junit:4.13.2")
+}
+
 java {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
---
+++ common/util/src/main/java/com/kappzzang/jeongsan/util/DateConverter.kt
@@ -0,0 +1,28 @@
+package com.kappzzang.jeongsan.util
+
+import java.sql.Timestamp
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+import java.util.Locale
+
+object DateConverter {
+    fun parseFromString(timeStamp: String): LocalDateTime {
+        // String을 Date로 변환
+        val date = Timestamp.valueOf(timeStamp)
+
+        // Date를 서울 타임존의 Instant로 변환
+        val instant = Instant.ofEpochMilli(date.time)
+            .atZone(ZoneId.systemDefault())
+
+        // Instant를 LocalDateTime으로 변환
+        return instant.toLocalDateTime()
+    }
+
+    fun LocalDateTime.formatToTransferString(): String =
+        this.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss", Locale.KOREAN))
+
+    fun LocalDateTime.formatToExpenseDate(): String =
+        this.format(DateTimeFormatter.ofPattern("MM/dd HH:mm", Locale.KOREAN))
+}
