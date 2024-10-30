0058a79430985ea64cf84699ed826bfbb19b825b
ksc1008 <ksc1008@naver.com>
2024-10-30 15:01:09 +0900

    Style: API Path 내 파라미터 명 수정

--- data/expense/src/main/java/com/kappzzang/jeongsan/api/ReceiptRetrofitService.kt
+++ data/expense/src/main/java/com/kappzzang/jeongsan/api/ReceiptRetrofitService.kt
@@ -4,16 +4,23 @@ import com.kappzzang.jeongsan.entity.ResponseWithExpenseIdDTO
 import com.kappzzang.jeongsan.entity.SaveExpensePayloadDTO
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface ReceiptRetrofitService {
-    @POST("/api/receipts/{groupId}")
+    @POST("/api/receipts/{teamId}")
     suspend fun saveExpense(
-        @Path(value = "groupId") groupId: String,
+        @Path(value = "teamId") groupId: String,
         @Header("accessToken") jwt: String,
         @Body body: SaveExpensePayloadDTO
     ):
             Response<ResponseWithExpenseIdDTO>
+
+    @GET("/api/receipts/items/{expenseId}")
+    suspend fun getExpenseDetails(
+        @Path(value = "expenseId") expenseId: String,
+        @Header("accessToken") jwt: String
+    )
 }
