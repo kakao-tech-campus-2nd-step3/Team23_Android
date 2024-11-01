1d541c622eec2f2e343eb965600918c41be9420e
ksc1008 <ksc1008@naver.com>
2024-11-01 16:07:12 +0900

    Refactor: ExpenseDetailEntity 새로운 패키지로 이동

--- data/expense/src/main/java/com/kappzzang/jeongsan/entity/expensedetail/ExpenseDetailEntity.kt
+++ data/expense/src/main/java/com/kappzzang/jeongsan/entity/expensedetail/ExpenseDetailEntity.kt
@@ -1,4 +1,4 @@
-package com.kappzzang.jeongsan.entity
+package com.kappzzang.jeongsan.entity.expensedetail
 
 import com.google.gson.annotations.SerializedName
 import kotlinx.serialization.Serializable
