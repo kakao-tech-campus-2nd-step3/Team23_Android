baaf482250f2f6837d7f65b8c0c2bc379cc4a85e
sumin <cleonn7878@gmail.com>
2024-11-01 19:34:19 +0900

    Test: ExpenseDetailViewModel 테스트 코드 추가

---
+++ ui/expensedetail/src/test/java/com/kappzzang/jeongsan/expensedetail/ExpenseDetailViewModelTest.kt
@@ -0,0 +1,89 @@
+package com.kappzzang.jeongsan.expensedetail
+
+import android.util.Log
+import com.kappzzang.jeongsan.model.ExpenseDetailItem
+import com.kappzzang.jeongsan.model.ExpenseItem
+import com.kappzzang.jeongsan.usecase.EditExpenseDetailUseCase
+import com.kappzzang.jeongsan.usecase.GetExpenseDetailUseCase
+import com.kappzzang.jeongsan.usecase.GetExpenseUseCase
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.mockkStatic
+import io.mockk.unmockkAll
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.mockito.ArgumentMatchers.any
+
+@ExperimentalCoroutinesApi
+class ExpenseDetailViewModelTest {
+    private val getExpenseDetailUseCase = mockk<GetExpenseDetailUseCase>()
+    private val getExpenseUseCase = mockk<GetExpenseUseCase>()
+    private val editExpenseDetailUseCase = mockk<EditExpenseDetailUseCase>()
+    private lateinit var viewModel: ExpenseDetailViewModel
+
+    private val testDispatcher = StandardTestDispatcher()
+
+    @Before
+    fun setUp() {
+        mockkStatic(Log::class)
+        every { Log.d(any(), any()) } returns 0
+        every { Log.e(any(), any()) } returns 0
+        coEvery { getExpenseUseCase(any()) } returns mockk()
+        coEvery { getExpenseDetailUseCase() } returns emptyList()
+        Dispatchers.setMain(testDispatcher)
+
+        viewModel = ExpenseDetailViewModel(
+            getExpenseDetailUseCase = getExpenseDetailUseCase,
+            getExpenseUseCase = getExpenseUseCase,
+            editExpenseDetailUseCase = editExpenseDetailUseCase,
+            ioDispatcher = testDispatcher
+        )
+    }
+
+    @After
+    fun tearDown() {
+        unmockkAll()
+        Dispatchers.resetMain()
+    }
+
+    @Test
+    fun `아이템 해제 시 비활성화 처리`() = runTest {
+        coEvery { getExpenseDetailUseCase() } returns listOf(
+            ExpenseDetailItem("testId", "testItem", 100, 30, 10)
+        )
+        viewModel.loadExpenseDetailList()
+        advanceUntilIdle()
+
+        viewModel.updateItemCheck(false, 0)
+        advanceUntilIdle()
+
+        assertEquals(0, viewModel.expenseDetailList.value[0].selectedQuantity)
+    }
+
+    @Test
+    fun `아이템 개수 변경시 업데이트`() = runTest {
+        coEvery { getExpenseDetailUseCase() } returns listOf(
+            ExpenseDetailItem("testId", "testItem", 100, 30, 1)
+        )
+        viewModel.loadExpenseDetailList()
+        advanceUntilIdle()
+
+        val testSelected = 10
+        viewModel.updateSelectedQuantity(testSelected, 0)
+        advanceUntilIdle()
+
+        assertEquals(testSelected, viewModel.expenseDetailList.value[0].selectedQuantity)
+    }
+}
