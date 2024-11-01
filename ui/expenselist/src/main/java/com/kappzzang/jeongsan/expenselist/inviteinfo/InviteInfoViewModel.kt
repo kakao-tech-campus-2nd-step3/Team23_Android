4210cda0bea891d9ca411f817cdc6c168b97c9aa
sumin <cleonn7878@gmail.com>
2024-11-01 19:45:19 +0900

    Refactor: InviteInfoViewModel Dispatcher DI 적용

--- ui/expenselist/src/main/java/com/kappzzang/jeongsan/expenselist/inviteinfo/InviteInfoViewModel.kt
+++ ui/expenselist/src/main/java/com/kappzzang/jeongsan/expenselist/inviteinfo/InviteInfoViewModel.kt
@@ -6,6 +6,7 @@ import com.kappzzang.jeongsan.model.MemberItem
 import com.kappzzang.jeongsan.usecase.GetInviteInfoUseCase
 import com.kappzzang.jeongsan.usecase.SendInviteMessageUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineDispatcher
 import javax.inject.Inject
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -15,7 +16,8 @@ import kotlinx.coroutines.launch
 @HiltViewModel
 class InviteInfoViewModel @Inject constructor(
     private val getInviteInfoUseCase: GetInviteInfoUseCase,
-    private val sendInviteMessageUseCase: SendInviteMessageUseCase
+    private val sendInviteMessageUseCase: SendInviteMessageUseCase,
+    private val ioDispatcher: CoroutineDispatcher
 ) : ViewModel() {
 
     private val _inviteInfo = MutableStateFlow<List<MemberItem>>(emptyList())
@@ -23,7 +25,7 @@ class InviteInfoViewModel @Inject constructor(
 
     init {
         // 더미 데이터 삽입 & 적용
-        viewModelScope.launch(Dispatchers.IO) {
+        viewModelScope.launch(ioDispatcher) {
             getInviteInfoUseCase.insertDummyData()
             _inviteInfo.emit(getInviteInfoUseCase())
         }
