8baf36b208073fe1b0ef97a9eea880dc9625c48b
Jeong Suhyeon <jsh00325@naver.com>
2024-11-01 19:09:52 +0900

    Refactor: 아직 리팩토링이 진행되지 않은 DI 모듈 수정

--- domain/expense/src/main/java/com/kappzzang/jeongsan/di/ExpenseUseCaseModule.kt
+++ domain/expense/src/main/java/com/kappzzang/jeongsan/di/ExpenseUseCaseModule.kt
@@ -3,9 +3,13 @@ package com.kappzzang.jeongsan.di
 import com.kappzzang.jeongsan.repository.ExpenseDetailRepository
 import com.kappzzang.jeongsan.repository.ExpenseRepository
 import com.kappzzang.jeongsan.repository.ReceiptRepository
+import com.kappzzang.jeongsan.repository.TransferRepository
+import com.kappzzang.jeongsan.repository.UserInfoRepository
 import com.kappzzang.jeongsan.usecase.EditExpenseDetailUseCase
 import com.kappzzang.jeongsan.usecase.GetExpenseDetailUseCase
 import com.kappzzang.jeongsan.usecase.GetExpenseUseCase
+import com.kappzzang.jeongsan.usecase.GetTransferInfoUseCase
+import com.kappzzang.jeongsan.usecase.SendTransferMessageUseCase
 import com.kappzzang.jeongsan.usecase.UploadExpenseUseCase
 import dagger.Module
 import dagger.Provides
@@ -30,4 +34,14 @@ object ExpenseUseCaseModule {
     @Provides
     fun provideUploadExpenseUseCase(receiptRepository: ReceiptRepository) =
         UploadExpenseUseCase(receiptRepository)
+
+    @Provides
+    fun provideGetTransferInfoUseCase(transferRepository: TransferRepository) =
+        GetTransferInfoUseCase(transferRepository)
+
+    @Provides
+    fun provideSendTransferMessageUseCase(
+        userInfoRepository: UserInfoRepository,
+        transferRepository: TransferRepository
+    ) = SendTransferMessageUseCase(userInfoRepository, transferRepository)
 }
