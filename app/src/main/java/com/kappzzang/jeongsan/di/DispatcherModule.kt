6caba5d114e0b7d06d4c54b2b5c6384e78c17421
정수현 <jsh00325@naver.com>
2024-11-01 12:07:31 +0900

    Test: 테스트 코드 작성 (#78)
    * Refactor: Unit Test를 위해 MainPageViewModel 수정
    
    - Test 단계에서 Test Dispatcher를 사용할 수 있도록, IO Dispatcher를 주입하는 것으로 수정
    - private 함수를 테스트하기 위해 public으로 수정
    
    * Test: MainPageViewModel을 위한 단위 테스트 작성
    
    * Test: AddExpenseViewModel을 위한 단위 테스트 작성
    
    * Test: CreateGroupViewModel을 위한 단위 테스트 작성
    
    * Test: SendMessageViewModel을 위한 단위 테스트 작성

---
+++ app/src/main/java/com/kappzzang/jeongsan/di/DispatcherModule.kt
@@ -0,0 +1,16 @@
+package com.kappzzang.jeongsan.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DispatcherModule {
+
+    @Provides
+    fun provideIoDispatcher(): CoroutineDispatcher = Dispatchers.IO
+}
