0a4ee42f949d2172538c77435daae4c6528e4534
ksc1008 <ksc1008@naver.com>
2024-11-02 15:23:04 +0900

    Feat: 변경된 Repository에 맞춰 ExpenseFakeRepositoryImpl 수정

--- data/expense/src/main/java/com/kappzzang/jeongsan/repositoryimpl/ExpenseFakeRepositoryImpl.kt
+++ data/expense/src/main/java/com/kappzzang/jeongsan/repositoryimpl/ExpenseFakeRepositoryImpl.kt
@@ -6,10 +6,12 @@ import com.kappzzang.jeongsan.model.ExpenseItem
 import com.kappzzang.jeongsan.model.ExpenseItemWithDetails
 import com.kappzzang.jeongsan.model.ExpenseListResponse
 import com.kappzzang.jeongsan.model.ExpenseState
+import com.kappzzang.jeongsan.model.ReceiptItem
 import com.kappzzang.jeongsan.repository.ExpenseRepository
 import javax.inject.Inject
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.last
 
 class ExpenseFakeRepositoryImpl @Inject constructor(
     private val dataSource: ExpenseListFakeDatasource
@@ -38,20 +40,13 @@ class ExpenseFakeRepositoryImpl @Inject constructor(
         )
     }
 
-    override suspend fun getExpense(id: Long) = ExpenseItemWithDetails(
-        item = ExpenseItem(
-            id = id.toString(),
-            state = ExpenseState.NOT_CONFIRMED,
-            name = "지출 이름입니당",
-            price = 15800
-        ),
-        // 임시 지출 이미지 주소 (카카오테크 캠퍼스)
-        expenseImageUrl = "https://www.kakaotechcampus.com/fileUpDownload/" +
-            "download.do?p_savefile=gatepage_20230330053504999_1.png&p_realfile=" +
-            "GNB+%EB%A1%9C%EA%B3%A0%28%EB%B3%B4%EB%9D%BC%29.png",
-        expenseDetails = listOf(
-            ExpenseDetailItem("", "1", 200, 1, 0),
-            ExpenseDetailItem("", "2", 300, 4, 1)
-        )
-    )
+    override suspend fun getExpenseListToGetPaid(groupId: String): ExpenseListResponse {
+        return dataSource.getExpenseData(
+            ExpenseState.TRANSFER_PENDING
+        ).last()
+    }
+
+    override suspend fun uploadExpense(receiptItem: ReceiptItem, groupId: String): String {
+        return dataSource.addExpense(receiptItem)
+    }
 }
