895623cf8d377f6800a41f1e828d6af808e6db69
권성찬 <54511614+ksc1008@users.noreply.github.com>
2024-11-06 12:02:41 +0900

    Feat: 로그인 우회 기능 구현 (#104)
    * Feat: Debug 환경에서 로그인을 우회할 수 있게 구현
    
    * Chore: Lint 수행

--- ui/login/src/main/java/com/kappzzang/jeongsan/login/LoginActivity.kt
+++ ui/login/src/main/java/com/kappzzang/jeongsan/login/LoginActivity.kt
@@ -12,6 +12,7 @@ import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.common.model.ClientError
 import com.kakao.sdk.common.model.ClientErrorCause
 import com.kakao.sdk.user.UserApiClient
+import com.kappzzang.jeongsan.build_config.BuildConfig
 import com.kappzzang.jeongsan.login.databinding.ActivityLoginBinding
 import com.kappzzang.jeongsan.navigation.MainPageNavigator
 import dagger.hilt.android.AndroidEntryPoint
@@ -39,6 +40,7 @@ class LoginActivity : AppCompatActivity() {
         }
         Log.d(TAG, intent?.data?.toString().toString())
         startCollectingKakaoLoginState()
+        createBypassLogin(binding)
 
         viewModel.login()
     }
@@ -98,6 +100,16 @@ class LoginActivity : AppCompatActivity() {
             }
         } else {
             UserApiClient.instance.loginWithKakaoAccount(this, callback = callback)
+        }
+    }
+
+    private fun createBypassLogin(binding: ActivityLoginBinding) {
+        if (BuildConfig.DEBUG) {
+            binding.loginByKakaoImagebutton.isLongClickable = true
+            binding.loginByKakaoImagebutton.setOnLongClickListener {
+                viewModel.bypassLogin()
+                true
+            }
         }
     }
 
