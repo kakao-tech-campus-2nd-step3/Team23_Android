9a936d3b436adc4adfb2a620421f8683c9e57417
Jeong Suhyeon <jsh00325@naver.com>
2024-11-07 19:19:32 +0900

    Feat: 401 에러 시 토큰을 새로 받아오는 로직 구현

---
+++ common/retrofit/src/main/java/com/kappzzang/jeongsan/retrofit/AuthInterceptor.kt
@@ -0,0 +1,46 @@
+package com.kappzzang.jeongsan.retrofit
+
+import com.kappzzang.jeongsan.util.AuthenticationRepository
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
+import okhttp3.Authenticator
+import okhttp3.Request
+import okhttp3.Response
+import okhttp3.Route
+import javax.inject.Inject
+
+class AuthInterceptor @Inject constructor(
+    private val authRepository: AuthenticationRepository
+) : Authenticator {
+    override fun authenticate(route: Route?, response: Response): Request? {
+        val originRequest = response.request()
+        if (originRequest.header(AUTH_HEADER_KEY).isNullOrEmpty()) {
+            return null
+        }
+
+        // 기존의 authData
+        val authData = runBlocking { authRepository.getServerAuthData().first() }
+
+        // 서버에 새로운 토큰을 요청
+        val newAuthData = runBlocking {
+            authRepository.refreshJwtFromServer(authData).getOrElse {
+                authRepository.removeServerAuthData()
+                return@runBlocking null
+            }
+        } ?: return null
+
+        // 새로운 토큰을 저장
+        runBlocking {
+            authRepository.updateServerAuthData(newAuthData)
+        }
+
+        return originRequest.newBuilder()
+            .removeHeader(AUTH_HEADER_KEY)
+            .addHeader(AUTH_HEADER_KEY, "Bearer ${newAuthData.accessToken}")
+            .build()
+    }
+
+    companion object {
+        private const val AUTH_HEADER_KEY = "Authorization"
+    }
+}
