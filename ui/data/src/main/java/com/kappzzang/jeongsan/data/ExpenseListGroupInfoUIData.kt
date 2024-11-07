6b9ccb6335b24891534c238d7b61f208aad99253
ksc1008 <ksc1008@naver.com>
2024-11-07 23:00:04 +0900

    Chore: Lint Format 수행

--- ui/data/src/main/java/com/kappzzang/jeongsan/data/ExpenseListGroupInfoUIData.kt
+++ ui/data/src/main/java/com/kappzzang/jeongsan/data/ExpenseListGroupInfoUIData.kt
@@ -1,9 +1,6 @@
 package com.kappzzang.jeongsan.data
 
-data class ExpenseListGroupInfoUIData(
-    val groupName: String,
-    val groupSubject: String
-) {
+data class ExpenseListGroupInfoUIData(val groupName: String, val groupSubject: String) {
     companion object {
         val EMPTY = ExpenseListGroupInfoUIData("", "")
     }
