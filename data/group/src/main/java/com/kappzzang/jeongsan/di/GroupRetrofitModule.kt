aa092532b662c8b51b926218e00687a455f8fa6c
sumin <cleonn7878@gmail.com>
2024-11-07 17:42:36 +0900

    Refactor: Lint 수행

--- data/group/src/main/java/com/kappzzang/jeongsan/di/GroupRetrofitModule.kt
+++ data/group/src/main/java/com/kappzzang/jeongsan/di/GroupRetrofitModule.kt
@@ -9,7 +9,6 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 @Module
 @InstallIn(SingletonComponent::class)
