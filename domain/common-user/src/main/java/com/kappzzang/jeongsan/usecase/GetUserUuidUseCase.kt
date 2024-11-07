b3f6eb690211dd2d6451102508f8b2f1438a82cd
Jeong Suhyeon <jsh00325@naver.com>
2024-11-08 01:01:40 +0900

    Chore: Lint 수행

--- domain/common-user/src/main/java/com/kappzzang/jeongsan/usecase/GetUserUuidUseCase.kt
+++ domain/common-user/src/main/java/com/kappzzang/jeongsan/usecase/GetUserUuidUseCase.kt
@@ -1,16 +1,16 @@
 package com.kappzzang.jeongsan.usecase
 
 import com.kappzzang.jeongsan.util.AuthenticationRepository
+import javax.inject.Inject
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
-import javax.inject.Inject
 
 class GetUserUuidUseCase @Inject constructor(
     private val authenticationRepository: AuthenticationRepository
 ) {
 
     // TODO: 추후에 EncryptedSharedPreferences를 사용하여 동기적으로 처리하기
-    operator fun invoke() : String = runBlocking {
+    operator fun invoke(): String = runBlocking {
         authenticationRepository.getUuid().first()
     }
 }
