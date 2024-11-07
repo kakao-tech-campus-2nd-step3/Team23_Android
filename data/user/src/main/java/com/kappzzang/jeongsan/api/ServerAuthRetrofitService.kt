e90db157aef609bb5291cc5f6a75003fd1c4eb3f
Jeong Suhyeon <jsh00325@naver.com>
2024-11-07 19:57:41 +0900

    Fix: User 모듈에서 토큰을 자동으로 삽입하지 않도록 수정

--- data/user/src/main/java/com/kappzzang/jeongsan/api/ServerAuthRetrofitService.kt
+++ data/user/src/main/java/com/kappzzang/jeongsan/api/ServerAuthRetrofitService.kt
@@ -7,16 +7,20 @@ import com.kappzzang.jeongsan.entity.RefreshTokenRequest
 import com.kappzzang.jeongsan.entity.RegisterRequest
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.Headers
 import retrofit2.http.POST
 
 interface ServerAuthRetrofitService {
 
     @POST("/api/members/token/refresh")
+    @Headers("Auth: false")
     suspend fun refreshToken(@Body request: RefreshTokenRequest): Response<RefreshResponse>
 
     @POST("/api/members/register")
+    @Headers("Auth: false")
     suspend fun register(@Body request: RegisterRequest): Response<AuthResponse>
 
     @POST("/api/members/login")
+    @Headers("Auth: false")
     suspend fun login(@Body request: LoginRequest): Response<AuthResponse>
 }
