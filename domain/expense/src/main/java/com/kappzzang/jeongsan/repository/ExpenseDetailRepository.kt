4b12937087adb0f380b02dd76fb2365f4b62e105
ksc1008 <ksc1008@naver.com>
2024-11-08 13:42:38 +0900

    Feat: ExpenseSelectionStatus 리포지토리 메소드 추가

--- domain/expense/src/main/java/com/kappzzang/jeongsan/repository/ExpenseDetailRepository.kt
+++ domain/expense/src/main/java/com/kappzzang/jeongsan/repository/ExpenseDetailRepository.kt
@@ -2,6 +2,7 @@ package com.kappzzang.jeongsan.repository
 
 import com.kappzzang.jeongsan.model.ExpenseDetailItem
 import com.kappzzang.jeongsan.model.ExpenseItemWithDetails
+import com.kappzzang.jeongsan.model.ExpenseSelectionStatus
 
 interface ExpenseDetailRepository {
     suspend fun getExpenseDetail(expenseId: String): Result<ExpenseItemWithDetails>
@@ -10,4 +11,6 @@ interface ExpenseDetailRepository {
         expenseId: String,
         groupId: String
     ): Result<Unit>
+
+    suspend fun getExpenseSelectionStatus(expenseId: String): Result<ExpenseSelectionStatus>
 }
