605f100a8d46dd9e2233b4653e86612678f298a9
ksc1008 <ksc1008@naver.com>
2024-11-10 00:03:30 +0900

    Feat: GroupInfo 엔티티에 kakaoId Nullable 프로퍼티 추가

--- data/group/src/main/java/com/kappzzang/jeongsan/entity/GroupInfo.kt
+++ data/group/src/main/java/com/kappzzang/jeongsan/entity/GroupInfo.kt
@@ -7,6 +7,8 @@ data class GroupInfo(
     val id: Long,
     @SerializedName("name")
     val name: String,
+    @SerializedName("ownerKakaoId")
+    val ownerServiceId: Int?,
     @SerializedName("isCompleted")
     val isCompleted: Boolean,
     @SerializedName("subject")
