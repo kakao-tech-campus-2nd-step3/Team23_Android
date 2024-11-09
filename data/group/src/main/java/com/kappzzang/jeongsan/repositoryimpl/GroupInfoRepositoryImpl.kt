9402a0d65effdfdbc585252652d43a4a5fda37eb
ksc1008 <ksc1008@naver.com>
2024-11-09 15:00:30 +0900

    Fix: 임의로 임시조치한 부분에 대한 TODO문 생성

--- data/group/src/main/java/com/kappzzang/jeongsan/repositoryimpl/GroupInfoRepositoryImpl.kt
+++ data/group/src/main/java/com/kappzzang/jeongsan/repositoryimpl/GroupInfoRepositoryImpl.kt
@@ -58,6 +58,9 @@ class GroupInfoRepositoryImpl @Inject constructor(
         val result = groupRemoteDataSource.createGroup(
             groupName = createdGroup.name,
             groupSubject = createdGroup.subject,
+            // groupMemberUuidList = createdGroup.memberUuidList
+            // TODO 멤버 UUID가 null로 들어가 있어서 제대로 처리가 안되는 오류가 있다고 합니다.
+            // TODO 서버 내에 저장된 더미 데이터인 UUID 1 값을 넣게 임시조치 했습니다.
             groupMemberUuidList = listOf("1")
         )
         return result.getOrThrow()
