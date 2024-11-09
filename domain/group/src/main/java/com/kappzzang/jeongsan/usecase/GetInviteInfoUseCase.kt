91cccad280cb0d31e675880571b5d55cea78f7cf
Jeong Suhyeon <jsh00325@naver.com>
2024-11-09 19:58:51 +0900

    Feat: 초대 현황에서 정상적으로 사용자 프로필을 띄우도록 수정

--- domain/group/src/main/java/com/kappzzang/jeongsan/usecase/GetInviteInfoUseCase.kt
+++ domain/group/src/main/java/com/kappzzang/jeongsan/usecase/GetInviteInfoUseCase.kt
@@ -6,18 +6,4 @@ import com.kappzzang.jeongsan.repository.MemberRepository
 class GetInviteInfoUseCase(private val memberRepository: MemberRepository) {
     suspend operator fun invoke(groupId: String): List<MemberItem> =
         memberRepository.getAllMember(groupId)
-
-    // 더미 데이터 삽입용 임시 함수
-    suspend fun insertDummyData() {
-//        for (i in 1..10) {
-//            memberRepository.addMember(
-//                MemberItem(
-//                    id = "id$i",
-//                    name = "멤버 이름$i",
-//                    profileImageUrl = "",
-//                    isInvited = i % 2 != 0
-//                )
-//            )
-//        }
-    }
 }
