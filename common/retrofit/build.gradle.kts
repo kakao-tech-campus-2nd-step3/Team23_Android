4f0a339fb056b9d1dc805a4d3c3bf4075fe43382
권성찬 <54511614+ksc1008@users.noreply.github.com>
2024-11-09 18:37:43 +0900

    Feat: Logging Interceptor 추가 (#123)
    * Feat: Logging Interceptor 추가
    
    * Chore: Lint Format 수행

--- common/retrofit/build.gradle.kts
+++ common/retrofit/build.gradle.kts
@@ -15,7 +15,7 @@ dependencies {
     implementation(project(":common:androidutil"))
     kapt("com.google.dagger:hilt-compiler:2.48.1")
     implementation("androidx.core:core-ktx:1.13.1")
-
+    implementation("com.squareup.okhttp3:logging-interceptor:4.9.2")
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
     implementation(project(":build-config"))
