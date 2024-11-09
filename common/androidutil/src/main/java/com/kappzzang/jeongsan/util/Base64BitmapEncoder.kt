1fc3e7757546db59f65f500b7f31d574a2d06328
ksc1008 <ksc1008@naver.com>
2024-11-09 17:19:36 +0900

    Fix: Base64 인코딩 시 파일 압축하도록 수정

--- common/androidutil/src/main/java/com/kappzzang/jeongsan/util/Base64BitmapEncoder.kt
+++ common/androidutil/src/main/java/com/kappzzang/jeongsan/util/Base64BitmapEncoder.kt
@@ -9,14 +9,37 @@ import android.provider.MediaStore
 import java.io.ByteArrayOutputStream
 import java.util.Base64
 
+
 object Base64BitmapEncoder {
     fun convertBitmapToBase64String(bitmap: Bitmap): String {
+        val resized = resizeBitmap(bitmap, 1024)
         val byteArrayOutputStream = ByteArrayOutputStream()
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream)
+        resized.compress(Bitmap.CompressFormat.JPEG, 50, byteArrayOutputStream)
         val imageBytes = byteArrayOutputStream.toByteArray()
         val encoded = Base64.getEncoder().encodeToString(imageBytes)
 
         return encoded
+    }
+
+    private fun calculateInSampleSize(width: Int, height: Int, sampleDimen: Int): Int {
+        var inSampleSize = 1
+        while (width / inSampleSize > sampleDimen || height / inSampleSize > sampleDimen) {
+            inSampleSize++
+        }
+        return inSampleSize
+    }
+
+    private fun resizeBitmap(bitmap: Bitmap, sampleDimen: Int): Bitmap {
+        val width = bitmap.width
+        val height = bitmap.height
+        val inSampleSize = (calculateInSampleSize(width, height, sampleDimen))
+        if (inSampleSize == 1) {
+            return bitmap
+        }
+        val sampleWidth = bitmap.width / inSampleSize
+        val sampleHeight = bitmap.height / inSampleSize
+        val result = Bitmap.createScaledBitmap(bitmap, sampleWidth, sampleHeight, true)
+        return result
     }
 
     fun convertUriToBitmap(uri: Uri, context: Context): Bitmap = if (Build.VERSION.SDK_INT >= 28) {
