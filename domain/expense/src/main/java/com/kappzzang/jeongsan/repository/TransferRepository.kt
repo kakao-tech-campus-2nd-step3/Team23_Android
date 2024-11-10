159cde44a80771989da35e4454d9fd0c3c2d5585
ksc1008 <ksc1008@naver.com>
2024-11-11 01:35:25 +0900

    Feat: SendTransferMessageUseCase 추가

--- domain/expense/src/main/java/com/kappzzang/jeongsan/repository/TransferRepository.kt
+++ domain/expense/src/main/java/com/kappzzang/jeongsan/repository/TransferRepository.kt
@@ -1,20 +1,13 @@
 package com.kappzzang.jeongsan.repository
 
-import com.kappzzang.jeongsan.model.ExpenseItem
 import com.kappzzang.jeongsan.model.TransferDetailItem
 
 interface TransferRepository {
-    suspend fun getTransferInfo(
-        groupId: String,
-        expenseIdList: List<String>
-    ): Result<List<TransferDetailItem>>
-
+    suspend fun getTransferInfo(): List<TransferDetailItem>
     suspend fun getTransferLink(memberUuid: String): String?
     suspend fun sendTransferMessage(
-        transferInfoList: List<TransferDetailItem>,
+        friendUuidList: List<String>,
         transferLink: String,
         payeeName: String
-    ): Boolean
-
-    suspend fun getPurchasedExpenseList(groupId: String): Result<List<ExpenseItem>>
+    ): Result<Unit>
 }
--- domain/expense/src/main/java/com/kappzzang/jeongsan/usecase/SendTransferMessageUseCase.kt
+++ domain/expense/src/main/java/com/kappzzang/jeongsan/usecase/SendTransferMessageUseCase.kt
@@ -1,6 +1,7 @@
 package com.kappzzang.jeongsan.usecase
 
 import com.kappzzang.jeongsan.model.TransferDetailItem
+import com.kappzzang.jeongsan.model.UserFriendItem
 import com.kappzzang.jeongsan.repository.TransferRepository
 import com.kappzzang.jeongsan.repository.UserInfoRepository
 import javax.inject.Inject
@@ -9,14 +10,35 @@ class SendTransferMessageUseCase @Inject constructor(
     private val userInfoRepository: UserInfoRepository,
     private val transferRepository: TransferRepository
 ) {
-    suspend operator fun invoke(transferInfoList: List<TransferDetailItem>): Boolean {
-        val requestUser = userInfoRepository.getUserInfo() ?: return false
-        val transferLink = transferRepository.getTransferLink(requestUser.serviceId) ?: return false
+    private fun mapServiceIdToUuid(
+        serviceIdList: List<String>,
+        friends: List<UserFriendItem>
+    ): List<String> =
+        serviceIdList.mapNotNull { serviceId ->
+            friends.find { friend ->
+                friend.serviceId == serviceId
+            }?.uuid
+        }
+
+    suspend operator fun invoke(transferInfoList: List<TransferDetailItem>): Result<Unit> {
+        val requestUser = userInfoRepository.getUserInfo() ?: return Result.failure(
+            IllegalStateException("유저 정보를 찾을 수 없습니다.")
+        )
+        val friends = userInfoRepository.getFriendList()
+            ?: return Result.failure(
+                IllegalStateException("친구 리스트를 불러오는 데 실패했습니다.")
+            )
+        val transferLink =
+            transferRepository.getTransferLink(requestUser.serviceId)
+                ?: return Result.failure(
+                    IllegalStateException("송금 링크를 조회하는데 실패했습니다.")
+                )
+        val friendUuidList = mapServiceIdToUuid(transferInfoList.map { it.serviceId }, friends)
 
         return transferRepository.sendTransferMessage(
-            transferInfoList,
-            transferLink,
-            requestUser.name
+            transferLink = transferLink,
+            friendUuidList = friendUuidList,
+            payeeName = requestUser.name
         )
     }
 
