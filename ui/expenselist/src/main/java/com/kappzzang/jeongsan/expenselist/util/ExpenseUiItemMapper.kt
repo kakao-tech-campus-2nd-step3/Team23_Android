0152d242d88acecc8329e42b814a441896b22ac8
ksc1008 <ksc1008@naver.com>
2024-11-10 13:09:00 +0900

    Fix: ExpenseList 크래시 현상 수정

--- ui/expenselist/src/main/java/com/kappzzang/jeongsan/expenselist/util/ExpenseUiItemMapper.kt
+++ ui/expenselist/src/main/java/com/kappzzang/jeongsan/expenselist/util/ExpenseUiItemMapper.kt
@@ -3,6 +3,7 @@ package com.kappzzang.jeongsan.expenselist.util
 import com.kappzzang.jeongsan.data.ExpenseUiItem
 import com.kappzzang.jeongsan.expenselist.viewmodel.ExpenseListPageViewModel.Companion.CURRENCY_POSTFIX
 import com.kappzzang.jeongsan.model.ExpenseItemWithCategory
+import com.kappzzang.jeongsan.util.ColorParser
 import com.kappzzang.jeongsan.util.IntegerFormatter.formatDecimalSeparator
 
 object ExpenseUiItemMapper {
@@ -15,7 +16,7 @@ object ExpenseUiItemMapper {
             id = item.id,
             name = item.name,
             date = item.date,
-            categoryColor = item.categoryColor,
+            categoryColor = ColorParser.parseColor(item.categoryColor),
             price = "${item.price.formatDecimalSeparator()} $CURRENCY_POSTFIX"
         )
     }
