d875f9814317af475c12c0f593035abd5a4677e1
ksc1008 <ksc1008@naver.com>
2024-11-11 17:03:53 +0900

    Test: 깨지는 테스트 수정

--- ui/sendmessage/src/test/java/com/kappzzang/jeongsan/sendmessage/SendMessageViewModelTest.kt
+++ ui/sendmessage/src/test/java/com/kappzzang/jeongsan/sendmessage/SendMessageViewModelTest.kt
@@ -1,8 +1,13 @@
 package com.kappzzang.jeongsan.sendmessage
 
+import com.kappzzang.jeongsan.model.ExpenseItem
+import com.kappzzang.jeongsan.model.ExpenseState
 import com.kappzzang.jeongsan.model.TransferDetailItem
+import com.kappzzang.jeongsan.sendmessage.data.HasTransferInfo
+import com.kappzzang.jeongsan.usecase.GetPurchasedExpenseListUseCase
 import com.kappzzang.jeongsan.usecase.GetTransferInfoUseCase
 import com.kappzzang.jeongsan.usecase.SendTransferMessageUseCase
+import com.kappzzang.jeongsan.usecase.SetExpensesToCompleteUseCase
 import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.mockk
@@ -26,17 +31,21 @@ class SendMessageViewModelTest {
     private val mockGetTransferInfoUseCase = mockk<GetTransferInfoUseCase>()
     private val mockSendTransferMessageUseCase = mockk<SendTransferMessageUseCase>()
     private lateinit var viewModel: SendMessageViewModel
-
+    private val mockGetPurchasedExpenseListUseCase = mockk<GetPurchasedExpenseListUseCase>()
+    private val mockSetExpensesToCompleteUseCase = mockk<SetExpensesToCompleteUseCase>()
     private val testDispatcher = StandardTestDispatcher(TestCoroutineScheduler())
 
     @Before
     fun setUp() {
         Dispatchers.setMain(testDispatcher)
-        coEvery { mockGetTransferInfoUseCase() } returns emptyList()
-        coEvery { mockSendTransferMessageUseCase(any()) } returns true
+        coEvery { mockGetTransferInfoUseCase(any(), any()) } returns Result.success(emptyList())
+        coEvery { mockSendTransferMessageUseCase(any()) } returns Result.success(Unit)
         viewModel = SendMessageViewModel(
             mockGetTransferInfoUseCase,
-            mockSendTransferMessageUseCase
+            mockSendTransferMessageUseCase,
+            mockGetPurchasedExpenseListUseCase,
+            mockSetExpensesToCompleteUseCase,
+            testDispatcher
         )
     }
 
@@ -50,36 +59,43 @@ class SendMessageViewModelTest {
     fun `송금 정보가 올바르게 받아서 합계를 잘 계산하는지 확인`() = runTest {
         // given
         val expectedTransferInfo = listOf(
-            TransferDetailItem("1", "test item 1", 2024, "test image url 1"),
-            TransferDetailItem("2", "test item 2", 11, "test image url 2"),
-            TransferDetailItem("3", "test item 3", 1, "test image url 3")
+            TransferDetailItem("1", "12", "test item 1", 2024, "test image url 1"),
+            TransferDetailItem("2", "22", "test item 2", 11, "test image url 2"),
+            TransferDetailItem("3", "32", "test item 3", 1, "test image url 3")
         )
-        coEvery { mockGetTransferInfoUseCase() } returns expectedTransferInfo
+        coEvery { mockGetTransferInfoUseCase(any(), any()) } returns Result.success(
+            expectedTransferInfo
+        )
+
+        coEvery { mockSendTransferMessageUseCase(any()) } returns Result.success(Unit)
+        coEvery { mockGetPurchasedExpenseListUseCase(any()) } returns Result.success(
+            listOf(
+                ExpenseItem("", "", 100, ExpenseState.TRANSFER_PENDING)
+            )
+        )
 
         // when
         viewModel = SendMessageViewModel(
             mockGetTransferInfoUseCase,
-            mockSendTransferMessageUseCase
+            mockSendTransferMessageUseCase,
+            mockGetPurchasedExpenseListUseCase,
+            mockSetExpensesToCompleteUseCase,
+            testDispatcher
         )
+        viewModel.setGroupId("123")
+        viewModel.startFetchUiState()
+
         advanceUntilIdle()
 
         // then
         val expectedTotalPrice = 2036
-        assertEquals(expectedTransferInfo, viewModel.transferInfo.value)
-        assertEquals(expectedTotalPrice, viewModel.totalPrice.value)
-    }
-
-    @Test
-    fun `sendTransferMessage가 성공적으로 호출되는지 확인`() = runTest {
-        // given
-        val transferInfo = viewModel.transferInfo.value
-
-        // when
-        val result = viewModel.sendTransferMessage()
-        advanceUntilIdle()
-
-        // then
-        assertEquals(true, result)
-        coVerify { mockSendTransferMessageUseCase(transferInfo) }
+        assertEquals(
+            expectedTransferInfo,
+            (viewModel.transferInfoState.value as? HasTransferInfo)?.transferInfoList
+        )
+        assertEquals(
+            expectedTotalPrice,
+            (viewModel.transferInfoState.value as? HasTransferInfo)?.totalExpenseToGet
+        )
     }
 }
