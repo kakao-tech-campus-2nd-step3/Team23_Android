f3a51c4966b69ba2ccdfea9e52acd745caf71adf
ksc1008 <ksc1008@naver.com>
2024-11-11 23:11:08 +0900

    Fix: 총 지출 텍스트 파싱 수정

--- ui/sendmessage/src/main/java/com/kappzzang/jeongsan/sendmessage/UiStateConverter.kt
+++ ui/sendmessage/src/main/java/com/kappzzang/jeongsan/sendmessage/UiStateConverter.kt
@@ -12,7 +12,7 @@ object UiStateConverter {
     @JvmStatic
     fun getSumTextFromState(state: TransferInfoUIState, context: Context): String =
         if (state is HasTransferInfo) {
-            "${(state.totalExpenseToGet)} ${context.getString(R.string.send_message_money_unit)}}"
+            "${(state.totalExpenseToGet)} ${context.getString(R.string.send_message_money_unit)}"
         } else {
             ""
         }
