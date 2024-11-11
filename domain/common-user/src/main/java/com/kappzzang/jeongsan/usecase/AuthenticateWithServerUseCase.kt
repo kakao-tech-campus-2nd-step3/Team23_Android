555e87aaa49fb9bf0bbff870d053b5c204e8b367
Jeong Suhyeon <jsh00325@naver.com>
2024-11-12 00:30:37 +0900

    Feat: 서비스 서버의 토큰을 받아오는 UseCase 구현

--- domain/common-user/src/main/java/com/kappzzang/jeongsan/usecase/AuthenticateWithServerUseCase.kt
+++ domain/common-user/src/main/java/com/kappzzang/jeongsan/usecase/AuthenticateWithServerUseCase.kt
@@ -1,49 +1,11 @@
 package com.kappzzang.jeongsan.usecase
 
-import com.kappzzang.jeongsan.data.ServerAuthData
-import com.kappzzang.jeongsan.repository.ServerAuthenticationRepository
 import com.kappzzang.jeongsan.util.AuthenticationRepository
 import javax.inject.Inject
 
 class AuthenticateWithServerUseCase @Inject constructor(
-    private val authenticationRepository: AuthenticationRepository,
-    private val serverAuthenticationRepository: ServerAuthenticationRepository
+    private val authenticationRepository: AuthenticationRepository
 ) {
 
-    suspend operator fun invoke(
-        serviceId: String,
-        nickname: String,
-        email: String,
-        profileImageUrl: String
-    ) {
-        val authData = attemptLoginOrRegister(serviceId, nickname, email, profileImageUrl)
-        authenticationRepository.updateServerAuthData(authData)
-        authenticationRepository.updateServiceId(serviceId)
-    }
-
-    private suspend fun attemptLoginOrRegister(
-        serviceId: String,
-        nickname: String,
-        email: String,
-        profileImageUrl: String
-    ): ServerAuthData = serverAuthenticationRepository.loginToServer(email).getOrElse { exception ->
-        when (exception) {
-            is NoSuchElementException -> registerToServer(
-                serviceId,
-                nickname,
-                email,
-                profileImageUrl
-            )
-            else -> throw exception
-        }
-    }
-
-    private suspend fun registerToServer(
-        serviceId: String,
-        nickname: String,
-        email: String,
-        profileImageUrl: String
-    ): ServerAuthData =
-        serverAuthenticationRepository.registerToServer(serviceId, nickname, email, profileImageUrl)
-            .getOrThrow()
+    operator fun invoke() = authenticationRepository.getServerAuthData()
 }
