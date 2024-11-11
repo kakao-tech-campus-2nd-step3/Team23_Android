7f3c7fcae1c547bd5c8cf952588171fffb7b1cfc
Jeong Suhyeon <jsh00325@naver.com>
2024-11-12 00:32:37 +0900

    Feat: 로그인 로직 변경

--- ui/login/src/main/java/com/kappzzang/jeongsan/login/LoginActivity.kt
+++ ui/login/src/main/java/com/kappzzang/jeongsan/login/LoginActivity.kt
@@ -2,6 +2,7 @@ package com.kappzzang.jeongsan.login
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Lifecycle
@@ -12,6 +13,7 @@ import com.kakao.sdk.common.model.ClientError
 import com.kakao.sdk.common.model.ClientErrorCause
 import com.kakao.sdk.user.UserApiClient
 import com.kappzzang.jeongsan.build_config.BuildConfig
+import com.kappzzang.jeongsan.data.AppLoginState
 import com.kappzzang.jeongsan.login.databinding.ActivityLoginBinding
 import com.kappzzang.jeongsan.navigation.MainPageNavigator
 import dagger.hilt.android.AndroidEntryPoint
@@ -24,33 +26,65 @@ class LoginActivity : AppCompatActivity() {
     lateinit var appNavigator: MainPageNavigator
 
     private val viewModel: LoginViewModel by viewModels()
+    private lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityLoginBinding.inflate(layoutInflater)
-
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         binding.viewModel = viewModel
         binding.lifecycleOwner = this
-
         setContentView(binding.root)
 
-        binding.loginByKakaoImagebutton.setOnClickListener {
-            loginWithKakao()
-        }
-        Log.d(TAG, intent?.data?.toString().toString())
-        startCollectingKakaoLoginState()
-        createBypassLogin(binding)
-
-        viewModel.login()
+        collectAppLoginState()
+        createBypassLogin()
     }
 
-    private fun startCollectingKakaoLoginState() {
+    private fun collectAppLoginState() {
         lifecycleScope.launch {
             repeatOnLifecycle(Lifecycle.State.STARTED) {
-                viewModel.loginStatus.collect {
-                    if (it == LoginStatus.LOGIN_COMPLETE) {
-                        navigateToMainPage()
-                        finish()
+                viewModel.appLoginStatus.collect { state ->
+                    Log.d(TAG, "AppLoginState: $state")
+                    when (state) {
+                        // 자동 로그인 시도
+                        is AppLoginState.TryAutoLogin -> {
+                            viewModel.checkKakaoTokenExist()
+                        }
+
+                        // 카카오 토큰이 없어서 로그인이 필요한 상태
+                        is AppLoginState.Idle.NotKakaoLoggedIn -> {
+                            binding.loginByKakaoImagebutton.setOnClickListener {
+                                loginWithKakao()
+                            }
+                        }
+
+                        // 서버 토큰이 없어서 로그인이 필요한 상태
+                        is AppLoginState.Idle.NotServerLoggedIn -> {
+                            binding.loginByKakaoImagebutton.setOnClickListener {
+                                viewModel.loginWithServer()
+                            }
+                        }
+
+                        // 카카오 로그인 실패
+                        is AppLoginState.KakaoLoginFailed -> {
+                            Log.d(TAG, "카카오 로그인 실패: ${state.message}")
+                            Toast.makeText(this@LoginActivity, state.message, Toast.LENGTH_SHORT)
+                                .show()
+                            viewModel.clearAppLoginStatus(AppLoginState.Idle.NotKakaoLoggedIn)
+                        }
+
+                        // 서버 로그인 실패
+                        is AppLoginState.ServerLoginFailed -> {
+                            Log.d(TAG, "서버 로그인 실패: ${state.message}")
+                            Toast.makeText(this@LoginActivity, state.message, Toast.LENGTH_SHORT)
+                                .show()
+                            viewModel.clearAppLoginStatus(AppLoginState.Idle.NotServerLoggedIn)
+                        }
+
+                        // 모든 로그인 완료
+                        is AppLoginState.LoginComplete -> {
+                            navigateToMainPage()
+                            finish()
+                        }
                     }
                 }
             }
@@ -97,7 +131,7 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 
-    private fun createBypassLogin(binding: ActivityLoginBinding) {
+    private fun createBypassLogin() {
         if (BuildConfig.DEBUG) {
             binding.loginByKakaoImagebutton.isLongClickable = true
             binding.loginByKakaoImagebutton.setOnLongClickListener {
