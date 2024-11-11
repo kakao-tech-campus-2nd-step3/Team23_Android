37891fc1a0e596c0c5981ad6927b84857a4e8b84
Jeong Suhyeon <jsh00325@naver.com>
2024-11-12 00:29:10 +0900

    Rename: 서비스 서버와 로그인이나 회원가입을 하는 UseCase 이름 변경

---
+++ domain/common-user/src/main/java/com/kappzzang/jeongsan/usecase/LoginOrRegisterUseCase.kt
@@ -0,0 +1,49 @@
+package com.kappzzang.jeongsan.usecase
+
+import com.kappzzang.jeongsan.data.ServerAuthData
+import com.kappzzang.jeongsan.repository.ServerAuthenticationRepository
+import com.kappzzang.jeongsan.util.AuthenticationRepository
+import javax.inject.Inject
+
+class LoginOrRegisterUseCase @Inject constructor(
+    private val authenticationRepository: AuthenticationRepository,
+    private val serverAuthenticationRepository: ServerAuthenticationRepository
+) {
+
+    suspend operator fun invoke(
+        serviceId: String,
+        nickname: String,
+        email: String,
+        profileImageUrl: String
+    ) {
+        val authData = attemptLoginOrRegister(serviceId, nickname, email, profileImageUrl)
+        authenticationRepository.updateServerAuthData(authData)
+        authenticationRepository.updateServiceId(serviceId)
+    }
+
+    private suspend fun attemptLoginOrRegister(
+        serviceId: String,
+        nickname: String,
+        email: String,
+        profileImageUrl: String
+    ): ServerAuthData = serverAuthenticationRepository.loginToServer(email).getOrElse { exception ->
+        when (exception) {
+            is NoSuchElementException -> registerToServer(
+                serviceId,
+                nickname,
+                email,
+                profileImageUrl
+            )
+            else -> throw exception
+        }
+    }
+
+    private suspend fun registerToServer(
+        serviceId: String,
+        nickname: String,
+        email: String,
+        profileImageUrl: String
+    ): ServerAuthData =
+        serverAuthenticationRepository.registerToServer(serviceId, nickname, email, profileImageUrl)
+            .getOrThrow()
+}
