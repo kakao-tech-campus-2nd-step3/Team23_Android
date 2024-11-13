4e971453b1473646e5fc2d00d3b21ce68fe55a58
정수현 <jsh00325@naver.com>
2024-11-13 16:38:16 +0900

    Design: 지출 목록 / 지출 상세 내역 디자인 수정 (#164)
    * Feat: 지출 이름의 길이가 긴 경우 겹치는 문제 해결
    
    * Feat: 지출 상세 내역 탭에서 지출 명이 잘리는 현상 수정
    
    * Feat: 지출 선택 현황에서 지출 아이템의 개수를 표시하도록 수정
    
    * Chore: Lint 수행
    
    * Feat: 다크 모드인 경우에도 의도한 UI가 보이도록 수정
    
    * Design: 지출 날짜를 아래로 조금 내리는 방식으로 수정
    
    * Design: 송금 대기 탭에서 지출 목록이 잘 보이도록 수정

--- app/src/main/java/com/kappzzang/jeongsan/JeongsanApplication.kt
+++ app/src/main/java/com/kappzzang/jeongsan/JeongsanApplication.kt
@@ -2,6 +2,7 @@ package com.kappzzang.jeongsan
 
 import android.app.Application
 import android.util.Log
+import androidx.appcompat.app.AppCompatDelegate
 import com.kakao.sdk.common.KakaoSdk
 import com.kappzzang.jeongsan.build_config.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
@@ -12,6 +13,9 @@ class JeongsanApplication : Application() {
         super.onCreate()
 
         KakaoSdk.init(this, BuildConfig.KAKAO_API_KEY)
+
+        // 다크 모드인 경우에도 라이트 모드로 설정하여 의도한 UI를 보여줄 수 있도록 함
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
         printHashKey()
     }
