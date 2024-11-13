417d14aaeabae7600673388126f206d1d1b2b799
ksc1008 <ksc1008@naver.com>
2024-11-13 13:34:12 +0900

    Fix: 더 이상 그룹 생성 시 테스트 계정을 초대하지 않게 수정

--- data/group/src/main/java/com/kappzzang/jeongsan/datasource/GroupRemoteDataSource.kt
+++ data/group/src/main/java/com/kappzzang/jeongsan/datasource/GroupRemoteDataSource.kt
@@ -1,7 +1,6 @@
 package com.kappzzang.jeongsan.datasource
 
 import com.kappzzang.jeongsan.api.GroupRetrofitService
-import com.kappzzang.jeongsan.build_config.BuildConfig
 import com.kappzzang.jeongsan.entity.CompleteGroupResponse
 import com.kappzzang.jeongsan.entity.CreateGroupRequestDTO
 import com.kappzzang.jeongsan.entity.CreateGroupResponse
