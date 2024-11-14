30b553ebbca17f0981a11969971af1e29d96de9a
ksc1008 <ksc1008@naver.com>
2024-11-14 14:05:25 +0900

    Setting: 앱 버전 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.kappzzang.jeongsan"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
